58c646851695d05c2d3ad02f8d9658d2ec7bdf49
zHd4 <computersystembase@gmail.com>
2020-09-28 18:15:40 +0300

    Updated StorageController eraseFile()

--- app/src/main/java/com/notesr/controllers/StorageController.java
+++ app/src/main/java/com/notesr/controllers/StorageController.java
@@ -10,7 +10,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.Arrays;
+import java.util.Random;
 
 public class StorageController extends Application {
 
@@ -31,7 +31,7 @@ public class StorageController extends Application {
         return file.exists();
     }
 
-    public static String readFile(Context context, String filename) {
+    public static String readFile(Context context, final String filename) {
         String result = "";
 
         File file = new File(context.getFilesDir(), Config.internalStoragePath);
@@ -57,7 +57,7 @@ public class StorageController extends Application {
         return result;
     }
 
-    public static void writeFile(Context context, String filename, String data) {
+    public static void writeFile(Context context, final String filename, final String data) {
         File internalStorage = new File(context.getFilesDir(), Config.internalStoragePath);
 
         if(!internalStorage.exists()) {
@@ -77,7 +77,7 @@ public class StorageController extends Application {
         }
     }
 
-    public static void deleteFile(Context context, String filename) {
+    public static void deleteFile(Context context, final String filename) {
         File internalStorage = new File(context.getFilesDir(), Config.internalStoragePath);
         File file = new File(internalStorage, filename);
 
@@ -101,7 +101,7 @@ public class StorageController extends Application {
         }
     }
 
-    public static boolean externalWriteFile(File file, String filedata) {
+    public static boolean externalWriteFile(File file, final String filedata) {
         try {
             FileOutputStream fstream = new FileOutputStream(file);
 
@@ -116,16 +116,23 @@ public class StorageController extends Application {
         }
     }
 
-    public static void eraseFile(Context context, String filename, int count) {
-        for(int i = 0; i <= count; i++) {
-            String emptyString = Arrays.toString(new char[1024]);
-            StorageController.writeFile(context, filename, emptyString);
+    public static void eraseFile(Context context, final String filename, final int cycles, final int size) {
+        final Random random = new Random();
+
+        for(int i = 0; i <= cycles; i++) {
+            StringBuilder floodData = new StringBuilder();
+
+            for(int iByte = 0; iByte <= size; iByte++) {
+                floodData.append((char)(byte)random.nextInt(256));
+            }
+
+            StorageController.writeFile(context, filename, floodData.toString());
         }
 
         StorageController.deleteFile(context, filename);
     }
 
     public static void eraseFile(Context context, String filename) {
-        eraseFile(context, filename, 15);
+        eraseFile(context, filename, 7, 2048);
     }
 }
