2e0ada1e75a8a6f0942584055e42fec6d9902b6d
zHd4 <computersystembase@gmail.com>
2023-11-27 21:18:57 +0100

    Create ExtendedAppCompatActivity

---
+++ app/src/main/java/com/notesr/controllers/activities/ExtendedAppCompatActivity.java
@@ -0,0 +1,15 @@
+package com.notesr.controllers.activities;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExtendedAppCompatActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
+                WindowManager.LayoutParams.FLAG_SECURE);
+    }
+}
