c1fc6cb00493da4e74a99a2a0813310736f08f50
zHd4 <computersystembase@gmail.com>
2023-11-27 21:22:17 +0100

    Add StartActivity

---
+++ app/src/main/java/com/notesr/controllers/activities/StartActivity.java
@@ -0,0 +1,13 @@
+package com.notesr.controllers.activities;
+
+import android.os.Bundle;
+
+import com.notesr.R;
+
+public class StartActivity extends ExtendedAppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
