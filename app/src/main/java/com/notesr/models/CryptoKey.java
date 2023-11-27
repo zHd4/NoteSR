bb317c13e6655e97e7c28278984a21543dab6e01
zHd4 <computersystembase@gmail.com>
2023-11-27 21:24:24 +0100

    Create CryptoKey

---
+++ app/src/main/java/com/notesr/models/CryptoKey.java
@@ -0,0 +1,22 @@
+package com.notesr.models;
+
+public class CryptoKey {
+    private byte[] key;
+    private String password;
+
+    public byte[] getKey() {
+        return key;
+    }
+
+    public void setKey(byte[] key) {
+        this.key = key;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
