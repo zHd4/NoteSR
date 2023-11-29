a5311403acdf9e1e4e2c61bcb5f56fa1eee56c89
zHd4 <computersystembase@gmail.com>
2023-11-29 21:41:46 +0100

    Create VersionFetcher

---
+++ app/src/main/java/com/peew/notesr/controllers/tools/VersionFetcher.java
@@ -0,0 +1,16 @@
+package com.peew.notesr.controllers.tools;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+public class VersionFetcher {
+    public static String fetchVersionName(Context context, boolean removeDot) throws
+            PackageManager.NameNotFoundException {
+        PackageManager packageManager = context.getPackageManager();
+        PackageInfo packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+
+        return removeDot ? packageInfo.versionName.replace(".", "") :
+                packageInfo.versionName;
+    }
+}
