ce110b15e263186851f7bccadf94a2a424ca10e0
zHd4 <computersystembase@gmail.com>
2023-11-30 21:00:26 +0100

    Refactor StartActivity

--- app/src/main/java/com/peew/notesr/activities/StartActivity.java
+++ app/src/main/java/com/peew/notesr/activities/StartActivity.java
@@ -8,6 +8,6 @@ public class StartActivity extends ExtendedAppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.start_activity);
     }
 }
