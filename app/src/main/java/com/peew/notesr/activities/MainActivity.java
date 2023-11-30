7b9f360c5f41ceb308c02a014b8472129c385220
zHd4 <computersystembase@gmail.com>
2023-11-30 21:03:50 +0100

    Add first run check to MainActivity

--- app/src/main/java/com/peew/notesr/activities/MainActivity.java
+++ app/src/main/java/com/peew/notesr/activities/MainActivity.java
@@ -1,7 +1,11 @@
 package com.peew.notesr.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.peew.notesr.App;
 import com.peew.notesr.R;
+import com.peew.notesr.crypto.CryptoManager;
 
 public class MainActivity extends ExtendedAppCompatActivity {
 
@@ -9,5 +13,9 @@ public class MainActivity extends ExtendedAppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(CryptoManager.getInstance().isFirstRun()) {
+            startActivity(new Intent(App.getContext(), StartActivity.class));
+        }
     }
 }
