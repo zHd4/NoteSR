a010c38c9b738d0b6e5802cb022954ab2cf6c8d6
zHd4 <computersystembase@gmail.com>
2023-11-30 18:32:01 +0100

    Transform CryptoManager to singleton and implement CryptoKey configuring

--- app/src/main/java/com/peew/notesr/crypto/CryptoManager.java
+++ app/src/main/java/com/peew/notesr/crypto/CryptoManager.java
@@ -1,8 +1,9 @@
 package com.peew.notesr.crypto;
 
 import android.content.Context;
+import android.util.Log;
 
-import com.peew.notesr.State;
+import com.peew.notesr.App;
 import com.peew.notesr.tools.FileManager;
 import com.peew.notesr.tools.HashHelper;
 
@@ -17,16 +18,49 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 public class CryptoManager {
+    private static final CryptoManager INSTANCE = new CryptoManager();
     private static final String MAIN_KEY_FILENAME = "key.enc";
     private static final String MAIN_SALT_FILENAME = "iv.enc";
-    private static final CryptoKey CRYPTO_KEY_INSTANCE = State.getInstance().getCryptoKeyInstance();
+    private CryptoKey cryptoKeyInstance;
 
-    private Context context;
+    private CryptoManager() {}
 
-    public CryptoManager(Context context) {
-        this.context = context;
+    public boolean configure(String password) {
+        try {
+            Context context = App.getContext();
+
+            File encryptedKeyFile = new File(context.getFilesDir(), MAIN_KEY_FILENAME);
+            File encryptedSaltFile = new File(context.getFilesDir(), MAIN_SALT_FILENAME);
+
+            byte[] encryptedKey = FileManager.readFileBytes(encryptedKeyFile);
+            byte[] encryptedSalt = FileManager.readFileBytes(encryptedSaltFile);
+
+            byte[] secondarySalt = HashHelper.toSha256Bytes(password.getBytes());
+            Aes256 aesInstance = new Aes256(password, secondarySalt);
+
+            byte[] mainKeyBytes = aesInstance.decrypt(encryptedKey);
+            byte[] mainSaltBytes = aesInstance.decrypt(encryptedSalt);
+
+            SecretKey mainKey = new SecretKeySpec(mainKeyBytes, 0, mainSaltBytes.length,
+                    Aes256.KEY_GENERATOR_ALGORITHM);
+            cryptoKeyInstance = new CryptoKey(mainKey, mainSaltBytes, password);
+
+            return true;
+        }catch (Exception e) {
+            Log.e("CryptoManager configuration error", e.toString());
+            return false;
+        }
+    }
+
+    public static CryptoManager getInstance() {
+        return INSTANCE;
+    }
+
+    public CryptoKey getCryptoKeyInstance() {
+        return cryptoKeyInstance;
     }
 
     public void createNewKey(String password) throws
@@ -34,14 +68,15 @@ public class CryptoManager {
             InvalidAlgorithmParameterException, NoSuchPaddingException,
             IllegalBlockSizeException, BadPaddingException,
             InvalidKeyException, IOException {
+        Context context = App.getContext();
         SecretKey mainKey = Aes256.generateRandomKey();
+
         byte[] mainSalt = Aes256.generateRandomSalt();
 
         byte[] secondarySalt = HashHelper.toSha256Bytes(password.getBytes());
         Aes256 aesInstance = new Aes256(password, secondarySalt);
 
-        CRYPTO_KEY_INSTANCE.setPassword(password);
-        CRYPTO_KEY_INSTANCE.setKey(mainKey);
+        cryptoKeyInstance = new CryptoKey(mainKey, mainSalt, password);
 
         File encryptedKeyFile = new File(context.getFilesDir(), MAIN_KEY_FILENAME);
         File encryptedSaltFile = new File(context.getFilesDir(), MAIN_SALT_FILENAME);
