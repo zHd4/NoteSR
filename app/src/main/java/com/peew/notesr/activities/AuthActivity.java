369c274cebf5654e2fc78eb04fa22fbb3db66b9b
zHd4 <computersystembase@gmail.com>
2023-12-01 17:57:09 +0100

    Create AuthActivity

---
+++ app/src/main/java/com/peew/notesr/activities/AuthActivity.java
@@ -0,0 +1,16 @@
+package com.peew.notesr.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.peew.notesr.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
