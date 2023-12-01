ecb8acd9f10622e794c7ecefc66eec3f548ed8a2
zHd4 <computersystembase@gmail.com>
2023-12-01 17:17:17 +0100

    Refactor SetupKeyActivity

--- app/src/main/java/com/peew/notesr/activities/SetupKeyActivity.java
+++ app/src/main/java/com/peew/notesr/activities/SetupKeyActivity.java
@@ -1,13 +1,10 @@
 package com.peew.notesr.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.peew.notesr.R;
 
-public class SetupKeyActivity extends AppCompatActivity {
-
+public class SetupKeyActivity extends ExtendedAppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
