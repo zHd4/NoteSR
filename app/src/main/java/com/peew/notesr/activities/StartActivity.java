5f061debeec441928fa12a7f55fec7cbeb84bc1b
zHd4 <computersystembase@gmail.com>
2023-12-01 16:18:18 +0100

    Implement StartActivity

--- app/src/main/java/com/peew/notesr/activities/StartActivity.java
+++ app/src/main/java/com/peew/notesr/activities/StartActivity.java
@@ -1,13 +1,21 @@
 package com.peew.notesr.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.peew.notesr.App;
 import com.peew.notesr.R;
 
 public class StartActivity extends ExtendedAppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.start_activity);
+        setContentView(R.layout.activity_start);
+
+        Intent setupKeyActivityIntent = new Intent(App.getContext(), SetupKeyActivity.class);
+        View.OnClickListener onGetStartedButtonClick = view -> startActivity(setupKeyActivityIntent);
+
+        findViewById(R.id.get_started_button).setOnClickListener(onGetStartedButtonClick);
     }
 }
