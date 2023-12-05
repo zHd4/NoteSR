965e0fef496ee675861abee700dd13726960baed
zHd4 <computersystembase@gmail.com>
2023-12-05 23:06:05 +0100

    Update CryptoKey

--- app/src/main/java/com/peew/notesr/crypto/CryptoKey.java
+++ app/src/main/java/com/peew/notesr/crypto/CryptoKey.java
@@ -1,5 +1,8 @@
 package com.peew.notesr.crypto;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 import javax.crypto.SecretKey;
 
 public class CryptoKey {
@@ -23,5 +26,24 @@ public class CryptoKey {
 
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CryptoKey cryptoKey = (CryptoKey) o;
+
+        return key.equals(cryptoKey.key) && Arrays.equals(salt, cryptoKey.salt) &&
+                password.equals(cryptoKey.password);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(key, password);
+        result = 31 * result + Arrays.hashCode(salt);
+
+        return result;
     }
 }
