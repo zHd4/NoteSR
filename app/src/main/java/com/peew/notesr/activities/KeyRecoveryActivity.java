b9572f9e7e8676915f0942902bdc1fbbfd3dd96c
zHd4 <computersystembase@gmail.com>
2023-12-08 00:30:54 +0100

    Create KeyRecoveryActivity

---
+++ app/src/main/java/com/peew/notesr/activities/KeyRecoveryActivity.java
@@ -0,0 +1,14 @@
+package com.peew.notesr.activities;
+
+import android.os.Bundle;
+
+import com.peew.notesr.R;
+
+public class KeyRecoveryActivity extends ExtendedAppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_key_recovery);
+    }
+}
