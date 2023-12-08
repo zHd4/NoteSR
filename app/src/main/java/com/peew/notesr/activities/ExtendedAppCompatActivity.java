68b07f4100b50fd19f959b5beeae0bfd3c92ea89
zHd4 <computersystembase@gmail.com>
2023-12-08 21:13:15 +0100

    Implement ExtendedAppCompatActivity.disableBackButton

--- app/src/main/java/com/peew/notesr/activities/ExtendedAppCompatActivity.java
+++ app/src/main/java/com/peew/notesr/activities/ExtendedAppCompatActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.Toast;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.activity.OnBackPressedDispatcher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.peew.notesr.App;
@@ -19,15 +21,25 @@ public class ExtendedAppCompatActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_SECURE);
     }
 
-    public void showToastMessage(String text, int duration) {
+    /** @noinspection SameParameterValue*/
+    protected void showToastMessage(String text, int duration) {
         Toast toast = Toast.makeText(App.getContext(), text, duration);
         toast.show();
     }
 
-    public void copyToClipboard(String label, String text) {
+    /** @noinspection SameParameterValue*/
+    protected void copyToClipboard(String label, String text) {
         ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
         ClipData clip = ClipData.newPlainText(label, text);
 
         clipboard.setPrimaryClip(clip);
+    }
+
+    protected void disableBackButton() {
+        OnBackPressedDispatcher dispatcher = getOnBackPressedDispatcher();
+        dispatcher.addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {}
+        });
     }
 }
