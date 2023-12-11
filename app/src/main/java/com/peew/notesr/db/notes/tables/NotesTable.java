14335c6f45f590cd1faf6787e6b79afe4d170136
zHd4 <computersystembase@gmail.com>
2023-12-11 09:02:53 +0100

    Implement NotesTable.getAllNotes

--- app/src/main/java/com/peew/notesr/db/notes/tables/NotesTable.java
+++ app/src/main/java/com/peew/notesr/db/notes/tables/NotesTable.java
@@ -1,13 +1,19 @@
 package com.peew.notesr.db.notes.tables;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.peew.notesr.db.notes.tables.models.Note;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public final class NotesTable extends Table {
     private final String name = "notes";
     private final Map<String, String> fields = Map.of(
-            "note_id", "INTEGER",
+            "note_id", "TEXT",
             "encrypted_name", "TEXT",
             "encrypted_data", "TEXT"
             );
@@ -24,5 +30,33 @@ public final class NotesTable extends Table {
     @Override
     public Map<String, String> getFields() {
         return fields;
+    }
+
+    public List<Note> getAllNotes() {
+        SQLiteDatabase db = helper.getReadableDatabase();
+        List<Note> notes = new ArrayList<>();
+
+        Cursor cursor = db.query(name,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null);
+
+        try (cursor) {
+            if (cursor.moveToFirst()) {
+                do {
+                    long id = Long.parseLong(decrypt(cursor.getString(0)));
+
+                    String name = decrypt(cursor.getString(1));
+                    String text = decrypt(cursor.getString(2));
+
+                    notes.add(new Note(id, name, text));
+                } while (cursor.moveToNext());
+            }
+        }
+
+        return notes;
     }
 }
