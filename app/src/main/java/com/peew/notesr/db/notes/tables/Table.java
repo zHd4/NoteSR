bb61f3be1d23cd055ed7bf6e80b08a26cf10fcd0
zHd4 <computersystembase@gmail.com>
2023-12-12 05:06:43 +0100

    Fix Table (exception in constructor)

--- app/src/main/java/com/peew/notesr/db/notes/tables/Table.java
+++ app/src/main/java/com/peew/notesr/db/notes/tables/Table.java
@@ -14,21 +14,19 @@ import java.util.Arrays;
 import java.util.Map;
 
 public abstract class Table {
-    private final Aes aesInstance;
+    private final CryptoKey cryptoKey;
     protected SQLiteOpenHelper helper;
     public abstract String getName();
     public abstract Map<String, String> getFields();
 
     public Table(SQLiteOpenHelper helper) {
-        CryptoKey cryptoKey = CryptoManager.getInstance().getCryptoKeyInstance();
-
         this.helper = helper;
-        this.aesInstance = new Aes(cryptoKey.getKey(), cryptoKey.getSalt());
+        this.cryptoKey = CryptoManager.getInstance().getCryptoKeyInstance();
     }
 
     protected String encrypt(String text) {
         try {
-            byte[] encrypted = aesInstance.encrypt(text.getBytes(StandardCharsets.UTF_8));
+            byte[] encrypted = getAesInstance().encrypt(text.getBytes(StandardCharsets.UTF_8));
             return Base64.encodeToString(encrypted, Base64.DEFAULT);
         } catch (Exception e) {
             Log.e("Table.encrypt error", e.toString());
@@ -39,10 +37,14 @@ public abstract class Table {
     protected String decrypt(String encryptedText) {
         try {
             byte[] decoded = Base64.decode(encryptedText, Base64.DEFAULT);
-            return Arrays.toString(aesInstance.decrypt(decoded));
+            return Arrays.toString(getAesInstance().decrypt(decoded));
         } catch (Exception e) {
             Log.e("Table.decrypt error", e.toString());
             throw new RuntimeException(e);
         }
+    }
+
+    private Aes getAesInstance() {
+        return new Aes(cryptoKey.getKey(), cryptoKey.getSalt());
     }
 }
