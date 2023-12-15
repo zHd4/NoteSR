82bca77125d5f582f6ade5dd7dd40e0f6c8c3cc0
zHd4 <computersystembase@gmail.com>
2023-12-15 22:58:39 +0100

    Update NoteOpenActivity

--- app/src/main/java/com/peew/notesr/activities/NoteOpenActivity.java
+++ app/src/main/java/com/peew/notesr/activities/NoteOpenActivity.java
@@ -1,6 +1,9 @@
 package com.peew.notesr.activities;
 
+import static androidx.core.view.inputmethod.EditorInfoCompat.IME_FLAG_NO_PERSONALIZED_LEARNING;
+
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.peew.notesr.R;
 
@@ -9,5 +12,11 @@ public class NoteOpenActivity extends ExtendedAppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_open);
+
+        EditText nameField = findViewById(R.id.note_name_field);
+        EditText textField = findViewById(R.id.note_text_field);
+
+        nameField.setImeOptions(IME_FLAG_NO_PERSONALIZED_LEARNING);
+        textField.setImeOptions(IME_FLAG_NO_PERSONALIZED_LEARNING);
     }
 }
