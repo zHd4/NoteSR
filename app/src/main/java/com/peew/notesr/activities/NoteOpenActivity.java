0300251f3c10edcf43a4af950a72bd16c1be0b61
zHd4 <computersystembase@gmail.com>
2023-12-15 22:38:45 +0100

    Create NoteOpenActivity

---
+++ app/src/main/java/com/peew/notesr/activities/NoteOpenActivity.java
@@ -0,0 +1,13 @@
+package com.peew.notesr.activities;
+
+import android.os.Bundle;
+
+import com.peew.notesr.R;
+
+public class NoteOpenActivity extends ExtendedAppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_open);
+    }
+}
