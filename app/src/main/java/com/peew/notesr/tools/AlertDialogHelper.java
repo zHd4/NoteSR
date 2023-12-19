80a82d7d84d7d69475f83e0dbd53cc269bc997cb
zHd4 <computersystembase@gmail.com>
2023-12-19 20:21:18 +0100

    Refactor AlertDialogHelper

--- app/src/main/java/com/peew/notesr/tools/AlertDialogHelper.java
+++ app/src/main/java/com/peew/notesr/tools/AlertDialogHelper.java
@@ -12,29 +12,46 @@ import com.peew.notesr.App;
 import com.peew.notesr.R;
 
 public class AlertDialogHelper {
-    public static AlertDialog generateYesNoDialog(Activity activity,
-                                                  String messageText,
-                                                  String positiveButtonText,
-                                                  String negativeButtonText,
-                                                  DialogInterface.OnClickListener onClickListener) {
+    public static AlertDialog generateYesNoDialog(Params params) {
         int dialogStyleId = R.style.AlertDialogTheme;
         int textColor = ContextCompat.getColor(App.getContext(), R.color.text_color);
 
         String dialogTextColor = toHexColor(textColor);
         String messageHtmlFormat = "<font color='%s'>%s</font>";
 
-        String message = String.format(messageHtmlFormat, dialogTextColor, messageText);
+        String message = String.format(messageHtmlFormat, dialogTextColor, params.messageText);
         Spanned messageSpanned = Html.fromHtml(message, Html.FROM_HTML_MODE_LEGACY);
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(activity, dialogStyleId);
+        AlertDialog.Builder builder = new AlertDialog.Builder(params.activity, dialogStyleId);
         builder.setMessage(messageSpanned)
-                .setPositiveButton(positiveButtonText, onClickListener)
-                .setNegativeButton(negativeButtonText, onClickListener);
+                .setPositiveButton(params.positiveButtonText, params.onClickListener)
+                .setNegativeButton(params.negativeButtonText, params.onClickListener);
 
         return builder.create();
     }
 
     private static String toHexColor(int color) {
         return "#" + Integer.toHexString(color).substring(2);
+    }
+
+    public static class Params {
+        private final Activity activity;
+        private final String messageText;
+        private final String positiveButtonText;
+        private final String negativeButtonText;
+        private final DialogInterface.OnClickListener onClickListener;
+
+        public Params(Activity activity,
+                      String messageText,
+                      String positiveButtonText,
+                      String negativeButtonText,
+                      DialogInterface.OnClickListener onClickListener) {
+
+            this.activity = activity;
+            this.messageText = messageText;
+            this.positiveButtonText = positiveButtonText;
+            this.negativeButtonText = negativeButtonText;
+            this.onClickListener = onClickListener;
+        }
     }
 }
