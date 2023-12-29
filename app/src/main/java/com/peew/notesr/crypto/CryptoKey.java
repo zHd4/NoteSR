26e188fbe6b1f5cdf6e2582ae152b953dc324681
zHd4 <computersystembase@gmail.com>
2023-12-29 23:59:07 +0100

    Covert CryptoKey to a record

--- app/src/main/java/com/peew/notesr/crypto/CryptoKey.java
+++ app/src/main/java/com/peew/notesr/crypto/CryptoKey.java
@@ -5,28 +5,7 @@ import java.util.Objects;
 
 import javax.crypto.SecretKey;
 
-public class CryptoKey {
-    private final SecretKey key;
-    private final byte[] salt;
-    private final String password;
-
-    public CryptoKey(SecretKey key, byte[] salt, String password) {
-        this.key = key;
-        this.salt = salt;
-        this.password = password;
-    }
-
-    public SecretKey getKey() {
-        return key;
-    }
-
-    public byte[] getSalt() {
-        return salt;
-    }
-
-    public String getPassword() {
-        return password;
-    }
+public record CryptoKey(SecretKey key, byte[] salt, String password) {
 
     @Override
     public boolean equals(Object o) {
