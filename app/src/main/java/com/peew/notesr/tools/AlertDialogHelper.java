4f3081255f38f6bf4f5219e36357f0642887ff6a
zHd4 <computersystembase@gmail.com>
2023-12-31 00:02:11 +0100

    Update AlertDialogHelper

--- app/src/main/java/com/peew/notesr/tools/AlertDialogHelper.java
+++ app/src/main/java/com/peew/notesr/tools/AlertDialogHelper.java
@@ -12,19 +12,27 @@ import com.peew.notesr.App;
 import com.peew.notesr.R;
 
 public class AlertDialogHelper {
-    public static AlertDialog generateYesNoDialog(Params params) {
+    public static AlertDialog.Builder generateSimpleDialogBuilder(Activity activity, String text) {
         int dialogStyleId = R.style.AlertDialogTheme;
         int textColor = ContextCompat.getColor(App.getContext(), R.color.text_color);
 
         String dialogTextColor = toHexColor(textColor);
         String messageHtmlFormat = "<font color='%s'>%s</font>";
 
-        String message = String.format(messageHtmlFormat, dialogTextColor, params.messageText);
+        String message = String.format(messageHtmlFormat, dialogTextColor, text);
         Spanned messageSpanned = Html.fromHtml(message, Html.FROM_HTML_MODE_LEGACY);
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(params.activity, dialogStyleId);
-        builder.setMessage(messageSpanned)
-                .setPositiveButton(params.positiveButtonText, params.onClickListener)
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity, dialogStyleId);
+        builder.setMessage(messageSpanned);
+
+        return builder;
+    }
+
+    public static AlertDialog generateYesNoDialog(YesNoDialogParams params) {
+        AlertDialog.Builder builder = generateSimpleDialogBuilder(
+                params.activity, params.messageText);
+
+        builder.setPositiveButton(params.positiveButtonText, params.onClickListener)
                 .setNegativeButton(params.negativeButtonText, params.onClickListener);
 
         return builder.create();
@@ -34,18 +42,18 @@ public class AlertDialogHelper {
         return "#" + Integer.toHexString(color).substring(2);
     }
 
-    public static class Params {
+    public static class YesNoDialogParams {
         private final Activity activity;
         private final String messageText;
         private final String positiveButtonText;
         private final String negativeButtonText;
         private final DialogInterface.OnClickListener onClickListener;
 
-        public Params(Activity activity,
-                      String messageText,
-                      String positiveButtonText,
-                      String negativeButtonText,
-                      DialogInterface.OnClickListener onClickListener) {
+        public YesNoDialogParams(Activity activity,
+                                 String messageText,
+                                 String positiveButtonText,
+                                 String negativeButtonText,
+                                 DialogInterface.OnClickListener onClickListener) {
 
             this.activity = activity;
             this.messageText = messageText;
