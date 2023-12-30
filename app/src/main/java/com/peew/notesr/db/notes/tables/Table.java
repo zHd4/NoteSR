af6faeb457be4d8c53e05fd41cc4a95275f9ebb5
zHd4 <computersystembase@gmail.com>
2023-12-30 23:15:40 +0100

    Update Table

--- app/src/main/java/com/peew/notesr/db/notes/tables/Table.java
+++ app/src/main/java/com/peew/notesr/db/notes/tables/Table.java
@@ -15,14 +15,25 @@ public abstract class Table {
     protected SQLiteOpenHelper helper;
     public abstract String getName();
     public abstract Map<String, String> getFields();
+    public abstract void reEncryptAll(CryptoKey oldCryptoKey);
 
     public Table(SQLiteOpenHelper helper) {
         this.helper = helper;
     }
 
     protected String encrypt(String text) {
+        return encrypt(text, CryptoManager.getInstance().getCryptoKeyInstance());
+    }
+
+    protected String decrypt(String text) {
+        return decrypt(text, CryptoManager.getInstance().getCryptoKeyInstance());
+    }
+
+    protected String encrypt(String text, CryptoKey cryptoKey) {
         try {
-            byte[] encrypted = getAesInstance().encrypt(text.getBytes(StandardCharsets.UTF_8));
+            Aes aesInstance = getAesInstance(cryptoKey);
+            byte[] encrypted = aesInstance.encrypt(text.getBytes(StandardCharsets.UTF_8));
+
             return Base64.encodeToString(encrypted, Base64.DEFAULT);
         } catch (Exception e) {
             Log.e("Table.encrypt error", e.toString());
@@ -30,18 +41,19 @@ public abstract class Table {
         }
     }
 
-    protected String decrypt(String encryptedText) {
+    protected String decrypt(String encryptedText, CryptoKey cryptoKey) {
         try {
+            Aes aesInstance = getAesInstance(cryptoKey);
             byte[] decoded = Base64.decode(encryptedText, Base64.DEFAULT);
-            return new String(getAesInstance().decrypt(decoded));
+
+            return new String(aesInstance.decrypt(decoded));
         } catch (Exception e) {
             Log.e("Table.decrypt error", e.toString());
             throw new RuntimeException(e);
         }
     }
 
-    private Aes getAesInstance() {
-        CryptoKey cryptoKey = CryptoManager.getInstance().getCryptoKeyInstance();
+    private Aes getAesInstance(CryptoKey cryptoKey) {
         return new Aes(cryptoKey.key(), cryptoKey.salt());
     }
 }
