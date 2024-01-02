e154a10e908d96c5866aa69fde21f10450307344
zHd4 <computersystembase@gmail.com>
2024-01-02 21:49:21 +0100

    Update SetupKeyActivity

--- app/src/main/java/com/peew/notesr/ui/setup/SetupKeyActivity.java
+++ app/src/main/java/com/peew/notesr/ui/setup/SetupKeyActivity.java
@@ -2,42 +2,25 @@ package com.peew.notesr.ui.setup;
 
 import static androidx.core.view.inputmethod.EditorInfoCompat.IME_FLAG_NO_PERSONALIZED_LEARNING;
 
-import android.app.AlertDialog;
-import android.content.Intent;
-import android.graphics.BlendMode;
-import android.graphics.BlendModeColorFilter;
-import android.graphics.ColorFilter;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.core.content.ContextCompat;
-
-import com.peew.notesr.App;
 import com.peew.notesr.R;
 import com.peew.notesr.crypto.CryptoKey;
 import com.peew.notesr.crypto.CryptoManager;
 import com.peew.notesr.crypto.CryptoTools;
-import com.peew.notesr.db.notes.NotesDatabase;
 import com.peew.notesr.ui.ExtendedAppCompatActivity;
-import com.peew.notesr.ui.MainActivity;
+import com.peew.notesr.ui.onclick.FinishKeySetupOnClick;
 
 import java.security.NoSuchAlgorithmException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class SetupKeyActivity extends ExtendedAppCompatActivity {
     public static final int FIRST_RUN_MODE = 0;
     public static final int REGENERATION_MODE = 1;
-    private int mode;
-    private String password;
-    private CryptoKey key;
     private final CryptoManager cryptoManager = CryptoManager.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,13 +28,15 @@ public class SetupKeyActivity extends ExtendedAppCompatActivity {
         setContentView(R.layout.activity_setup_key);
 
         TextView keyView = findViewById(R.id.aes_key_hex);
+
         EditText importKeyField = findViewById(R.id.import_key_field);
+        importKeyField.setImeOptions(IME_FLAG_NO_PERSONALIZED_LEARNING);
 
         Button copyToClipboardButton = findViewById(R.id.copy_aes_key_hex);
         Button nextButton = findViewById(R.id.key_setup_next_button);
 
-        mode = getIntent().getIntExtra("mode", -1);
-        password = getIntent().getStringExtra("password");
+        int mode = getIntent().getIntExtra("mode", -1);
+        String password = getIntent().getStringExtra("password");
 
         if (mode == -1) {
             throw new RuntimeException("Mode didn't provided");
@@ -63,7 +48,7 @@ public class SetupKeyActivity extends ExtendedAppCompatActivity {
             throw new RuntimeException("Password didn't provided");
         }
 
-        importKeyField.setImeOptions(IME_FLAG_NO_PERSONALIZED_LEARNING);
+        CryptoKey key;
 
         try {
             key = cryptoManager.generateNewKey(password);
@@ -74,81 +59,14 @@ public class SetupKeyActivity extends ExtendedAppCompatActivity {
         String keyHex = CryptoTools.cryptoKeyToHex(key);
 
         keyView.setText(keyHex);
-        copyToClipboardButton.setOnClickListener(copyToClipboardOnClick(keyHex));
-        nextButton.setOnClickListener(nextButtonOnClick(importKeyField));
+        copyToClipboardButton.setOnClickListener(getCopyKeyOnClick(keyHex));
+        nextButton.setOnClickListener(new FinishKeySetupOnClick(this, mode, password, key));
     }
 
-    private View.OnClickListener copyToClipboardOnClick(String keyHex) {
+    private View.OnClickListener getCopyKeyOnClick(String keyHex) {
         return view -> {
             copyToClipboard("", keyHex);
             showToastMessage(getString(R.string.copied), Toast.LENGTH_SHORT);
         };
-    }
-
-    private View.OnClickListener nextButtonOnClick(EditText importKeyField) {
-        return view -> {
-            String hexKeyToImport = importKeyField.getText().toString();
-
-            if (!hexKeyToImport.isBlank()) {
-                try {
-                    key = CryptoTools.hexToCryptoKey(hexKeyToImport, password);
-                } catch (Exception e) {
-                    Log.e("hexToCryptoKey" , e.toString());
-                    proceedKeyImportFail(importKeyField);
-                    return;
-                }
-            }
-
-            if (mode == FIRST_RUN_MODE) {
-                try {
-                    cryptoManager.applyNewKey(key);
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-
-                startActivity(new Intent(App.getContext(), MainActivity.class));
-            } else if (mode == REGENERATION_MODE) {
-                proceedRegeneration();
-            }
-        };
-    }
-
-    private void proceedRegeneration() {
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        Handler handler = new Handler(Looper.getMainLooper());
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogTheme);
-
-        executor.execute(() -> {
-            handler.post(() -> {
-                builder.setView(R.layout.progress_dialog_re_encryption);
-                builder.setCancelable(false);
-                builder.create().show();
-            });
-
-            try {
-                CryptoKey oldCryptoKey = cryptoManager.getCryptoKeyInstance().copy();
-                cryptoManager.applyNewKey(key);
-
-                NotesDatabase.getInstance().reEncryptAllTables(oldCryptoKey);
-
-                startActivity(new Intent(App.getContext(), MainActivity.class));
-                finish();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }
-
-    private void proceedKeyImportFail(EditText importKeyField) {
-        int importFailedColor = ContextCompat.getColor(
-                App.getContext(),
-                R.color.key_import_failed_color);
-
-        ColorFilter colorFilter = new BlendModeColorFilter(
-                importFailedColor,
-                BlendMode.SRC_ATOP);
-
-        importKeyField.getBackground().setColorFilter(colorFilter);
     }
 }
