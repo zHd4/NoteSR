4f495086e79d16511f5f7cb703806aa905d5c083
zHd4 <computersystembase@gmail.com>
2024-01-02 21:47:35 +0100

    Add NoteOnClick

---
+++ app/src/main/java/com/peew/notesr/ui/onclick/NoteOnClick.java
@@ -0,0 +1,27 @@
+package com.peew.notesr.ui.onclick;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+
+import com.peew.notesr.App;
+import com.peew.notesr.ui.ExtendedAppCompatActivity;
+import com.peew.notesr.ui.manage.NoteOpenActivity;
+
+public class NoteOnClick implements AdapterView.OnItemClickListener {
+    private final ExtendedAppCompatActivity activity;
+
+    public NoteOnClick(ExtendedAppCompatActivity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent noteOpenActivtyIntent = new Intent(App.getContext(), NoteOpenActivity.class);
+
+        noteOpenActivtyIntent.putExtra("mode", NoteOpenActivity.EDIT_NOTE_MODE);
+        noteOpenActivtyIntent.putExtra("note_id", id);
+
+        activity.startActivity(noteOpenActivtyIntent);
+    }
+}
