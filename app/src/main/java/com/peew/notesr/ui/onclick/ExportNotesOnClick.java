a441fec7bd6847b333e03f9dde63afbe0a4be7ef
zHd4 <computersystembase@gmail.com>
2024-01-04 18:08:15 +0100

    Update ExportNotesOnClick

--- app/src/main/java/com/peew/notesr/ui/onclick/ExportNotesOnClick.java
+++ app/src/main/java/com/peew/notesr/ui/onclick/ExportNotesOnClick.java
@@ -1,5 +1,8 @@
 package com.peew.notesr.ui.onclick;
 
+import android.widget.Toast;
+
+import com.peew.notesr.R;
 import com.peew.notesr.db.notes.NotesExporter;
 import com.peew.notesr.ui.MainActivity;
 
@@ -9,7 +12,10 @@ public class ExportNotesOnClick implements Consumer<MainActivity> {
     @Override
     public void accept(MainActivity activity) {
         try {
-            new NotesExporter(activity).export();
+            String path = new NotesExporter(activity).export();
+            String message = String.format(activity.getString(R.string.saved_to), path);
+
+            activity.showToastMessage(message, Toast.LENGTH_SHORT);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
