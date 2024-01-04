e2c9cda69fea6000ef4e06d42c233b1a5b6c60a6
zHd4 <computersystembase@gmail.com>
2024-01-05 00:10:16 +0100

    Update Note

--- app/src/main/java/com/peew/notesr/models/Note.java
+++ app/src/main/java/com/peew/notesr/models/Note.java
@@ -2,7 +2,21 @@ package com.peew.notesr.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Objects;
+
 public record Note(@JsonProperty("id") long id,
                    @JsonProperty("name") String name,
                    @JsonProperty("text") String text) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Note note = (Note) o;
+        return id == note.id && name.equals(note.name) && text.equals(note.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, text);
+    }
 }
