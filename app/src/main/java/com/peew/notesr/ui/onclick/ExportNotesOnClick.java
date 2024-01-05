a01567ec4c34c60169a88f0448cc4031bd3bb9fa
zHd4 <computersystembase@gmail.com>
2024-01-05 02:27:00 +0100

    Update ExportNotesOnClick

--- app/src/main/java/com/peew/notesr/ui/onclick/ExportNotesOnClick.java
+++ app/src/main/java/com/peew/notesr/ui/onclick/ExportNotesOnClick.java
@@ -1,23 +1,43 @@
 package com.peew.notesr.ui.onclick;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
 
 import com.peew.notesr.R;
 import com.peew.notesr.db.notes.NotesExporter;
 import com.peew.notesr.ui.MainActivity;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 
 public class ExportNotesOnClick implements Consumer<MainActivity> {
     @Override
     public void accept(MainActivity activity) {
-        try {
-            String path = new NotesExporter(activity).export();
-            String message = String.format(activity.getString(R.string.saved_to), path);
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity, R.style.AlertDialogTheme);
+        builder.setView(R.layout.progress_dialog_exporting).setCancelable(false);
 
-            activity.showToastMessage(message, Toast.LENGTH_SHORT);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        AlertDialog progressDialog = builder.create();
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        executor.execute(() -> {
+            handler.post(progressDialog::show);
+
+            try {
+                String path = new NotesExporter(activity).export();
+                String message = String.format(activity.getString(R.string.saved_to), path);
+
+                activity.runOnUiThread(() -> activity.showToastMessage(message, Toast.LENGTH_SHORT));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+            progressDialog.dismiss();
+        });
     }
 }
