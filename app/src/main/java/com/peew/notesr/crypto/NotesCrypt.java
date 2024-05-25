d4b919e74d57358342c62b74bb21e162483d2eee
zHd4 <computersystembase@gmail.com>
2024-05-26 00:14:57 +0200

    Update NotesCrypt

--- app/src/main/java/com/peew/notesr/crypto/NotesCrypt.java
+++ app/src/main/java/com/peew/notesr/crypto/NotesCrypt.java
@@ -9,29 +9,29 @@ import com.peew.notesr.model.Note;
 import java.nio.charset.StandardCharsets;
 
 public class NotesCrypt {
-    public EncryptedNote encrypt(Note note) {
+    public static EncryptedNote encrypt(Note note) {
         return encrypt(note, CryptoManager.getInstance().getCryptoKeyInstance());
     }
 
-    public Note decrypt(EncryptedNote encryptedNote) {
+    public static Note decrypt(EncryptedNote encryptedNote) {
         return decrypt(encryptedNote, CryptoManager.getInstance().getCryptoKeyInstance());
     }
 
-    public EncryptedNote encrypt(Note note, CryptoKey cryptoKey) {
+    public static EncryptedNote encrypt(Note note, CryptoKey cryptoKey) {
         String encryptedName = encryptValue(note.name(), cryptoKey);
         String encryptedText = encryptValue(note.text(), cryptoKey);
 
         return new EncryptedNote(note.id(), encryptedName, encryptedText);
     }
 
-    public Note decrypt(EncryptedNote encryptedNote, CryptoKey cryptoKey) {
+    public static Note decrypt(EncryptedNote encryptedNote, CryptoKey cryptoKey) {
         String name = decryptValue(encryptedNote.encryptedName(), cryptoKey);
         String text = decryptValue(encryptedNote.encryptedText(), cryptoKey);
 
         return new Note(encryptedNote.id(), name, text);
     }
 
-    private String encryptValue(String value, CryptoKey cryptoKey) {
+    private static String encryptValue(String value, CryptoKey cryptoKey) {
         try {
             Aes aesInstance = getAesInstance(cryptoKey);
             byte[] encrypted = aesInstance.encrypt(value.getBytes(StandardCharsets.UTF_8));
@@ -43,7 +43,7 @@ public class NotesCrypt {
         }
     }
 
-    private String decryptValue(String value, CryptoKey cryptoKey) {
+    private static String decryptValue(String value, CryptoKey cryptoKey) {
         try {
             Aes aesInstance = getAesInstance(cryptoKey);
             byte[] decoded = Base64.decode(value, Base64.DEFAULT);
@@ -55,7 +55,7 @@ public class NotesCrypt {
         }
     }
 
-    private Aes getAesInstance(CryptoKey cryptoKey) {
+    private static Aes getAesInstance(CryptoKey cryptoKey) {
         return new Aes(cryptoKey.key(), cryptoKey.salt());
     }
 }
