892e1e33c25601a4f9c45213d4749b836f930917
zHd4 <computersystembase@gmail.com>
2024-05-26 00:22:11 +0200

    Update NotesCrypt

--- app/src/main/java/com/peew/notesr/crypto/NotesCrypt.java
+++ app/src/main/java/com/peew/notesr/crypto/NotesCrypt.java
@@ -7,14 +7,32 @@ import com.peew.notesr.model.EncryptedNote;
 import com.peew.notesr.model.Note;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class NotesCrypt {
+    public static List<EncryptedNote> encrypt(List<Note> notes) {
+        return encrypt(notes, CryptoManager.getInstance().getCryptoKeyInstance());
+    }
+
+    public static List<Note> decrypt(List<EncryptedNote> notes) {
+        return decrypt(notes, CryptoManager.getInstance().getCryptoKeyInstance());
+    }
+
     public static EncryptedNote encrypt(Note note) {
         return encrypt(note, CryptoManager.getInstance().getCryptoKeyInstance());
     }
 
     public static Note decrypt(EncryptedNote encryptedNote) {
         return decrypt(encryptedNote, CryptoManager.getInstance().getCryptoKeyInstance());
+    }
+
+    public static List<EncryptedNote> encrypt(List<Note> notes, CryptoKey cryptoKey) {
+        return notes.stream().map(note -> encrypt(note, cryptoKey)).collect(Collectors.toList());
+    }
+
+    public static List<Note> decrypt(List<EncryptedNote> notes, CryptoKey cryptoKey) {
+        return notes.stream().map(note -> decrypt(note, cryptoKey)).collect(Collectors.toList());
     }
 
     public static EncryptedNote encrypt(Note note, CryptoKey cryptoKey) {
