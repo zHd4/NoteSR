8e2a9b53614d36827c55bc9916c0e28c82446953
zHd4 <computersystembase@gmail.com>
2024-05-27 21:51:06 +0200

    Refactor EncryptedNote

--- app/src/main/java/com/peew/notesr/model/EncryptedNote.java
+++ app/src/main/java/com/peew/notesr/model/EncryptedNote.java
@@ -1,3 +1,28 @@
 package com.peew.notesr.model;
 
-public record EncryptedNote(Long id, String encryptedName, String encryptedText) { }
+public final class EncryptedNote {
+    private Long id;
+    private final String encryptedName;
+    private final String encryptedText;
+
+    public EncryptedNote(String encryptedName, String encryptedText) {
+        this.encryptedName = encryptedName;
+        this.encryptedText = encryptedText;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEncryptedName() {
+        return encryptedName;
+    }
+
+    public String getEncryptedText() {
+        return encryptedText;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
