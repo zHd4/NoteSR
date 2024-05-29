ae70557f1385b1d964b083bcb1e9cf02270fd4b8
zHd4 <computersystembase@gmail.com>
2024-05-29 23:22:40 +0200

    Update App

--- app/src/main/java/com/peew/notesr/App.java
+++ app/src/main/java/com/peew/notesr/App.java
@@ -5,15 +5,22 @@ import android.content.Context;
 
 public class App extends Application {
     private static App context;
+    private static AppContainer appContainer;
 
     @Override
     public void onCreate() {
         super.onCreate();
+
         context = this;
+        appContainer = new AppContainer();
     }
 
     public static Context getContext() {
         return context;
+    }
+
+    public static AppContainer getAppContainer() {
+        return appContainer;
     }
 
     public static boolean onAndroid() {
