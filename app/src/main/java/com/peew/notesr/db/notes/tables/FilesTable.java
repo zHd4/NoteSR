c0d3ce3045b521bb19add1b90086ad1e0a62db85
zHd4 <computersystembase@gmail.com>
2024-06-01 02:02:46 +0200

    Update FilesTable.java

--- app/src/main/java/com/peew/notesr/db/notes/tables/FilesTable.java
+++ app/src/main/java/com/peew/notesr/db/notes/tables/FilesTable.java
@@ -1,6 +1,11 @@
 package com.peew.notesr.db.notes.tables;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import com.peew.notesr.model.EncryptedFile;
 
 public class FilesTable extends Table{
     public FilesTable(SQLiteOpenHelper helper, String name) {
@@ -10,5 +15,45 @@ public class FilesTable extends Table{
                         "id integer PRIMARY KEY AUTOINCREMENT, " +
                         "encrypted_name text NOT NULL, " +
                         "encrypted_data blob NOT NULL)");
+    }
+
+    public void save(EncryptedFile file) {
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put("encrypted_name", file.getEncryptedName());
+        values.put("encrypted_data", file.getEncryptedData());
+
+        if (file.getId() == null || get(file.getId()) == null) {
+            db.insert(name, null, values);
+        } else {
+            db.update(name, values, "id" + "=" + file.getId(), null);
+        }
+    }
+
+    public EncryptedFile get(long id) {
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        Cursor cursor = db.query(name,
+                new String[] { "encrypted_name", "encrypted_data" },
+                "id" + "=" + id,
+                null,
+                null,
+                null,
+                null);
+
+        try (cursor) {
+            if (cursor.moveToFirst()) {
+                String name = cursor.getString(0);
+                byte[] data = cursor.getBlob(1);
+
+                EncryptedFile file = new EncryptedFile(name, data);
+
+                file.setId(id);
+                return file;
+            }
+        }
+
+        return null;
     }
 }
