5fee704efe31c28d40d0839c024f2b2ea78f7f4d
zHd4 <computersystembase@gmail.com>
2024-06-04 01:08:36 +0200

    Update AssignmentsListActivity

--- app/src/main/java/com/peew/notesr/ui/manage/assignments/AssignmentsListActivity.java
+++ app/src/main/java/com/peew/notesr/ui/manage/assignments/AssignmentsListActivity.java
@@ -2,9 +2,17 @@ package com.peew.notesr.ui.manage.assignments;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.peew.notesr.App;
 import com.peew.notesr.R;
+import com.peew.notesr.crypto.FilesCrypt;
+import com.peew.notesr.db.notes.tables.FilesTable;
+import com.peew.notesr.model.File;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class AssignmentsListActivity extends AppCompatActivity {
 
@@ -12,5 +20,34 @@ public class AssignmentsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assignments_list);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle(R.string.files);
+
+//        fillTable();
     }
+
+//    private void fillTable() {
+//        FilesTable filesTable = App.getAppContainer().getNotesDatabase().getFilesTable();
+//
+//        if (filesTable.getByNoteId(1).isEmpty()) {
+//            File file1 = new File("file1.txt", "foo".getBytes(StandardCharsets.UTF_8));
+//            File file2 = new File("file2.txt", "bar".getBytes(StandardCharsets.UTF_8));
+//            File file3 = new File("file3.txt", "baz".getBytes(StandardCharsets.UTF_8));
+//
+//            file1.setNoteId(1L);
+//            file2.setNoteId(1L);
+//            file3.setNoteId(1L);
+//
+//            filesTable.save(FilesCrypt.encrypt(file1));
+//            filesTable.save(FilesCrypt.encrypt(file2));
+//            filesTable.save(FilesCrypt.encrypt(file3));
+//        }
+//
+//        List<File> files = FilesCrypt.decrypt(filesTable.getByNoteId(1L));
+//        System.out.println();
+//    }
 }
