1966c05fd78910b2c2039801f4370ba4be77d597
zHd4 <computersystembase@gmail.com>
2024-06-14 01:09:07 +0200

    Update AssignmentsListActivity

--- app/src/main/java/com/peew/notesr/activity/files/AssignmentsListActivity.java
+++ app/src/main/java/com/peew/notesr/activity/files/AssignmentsListActivity.java
@@ -70,16 +70,6 @@ public class AssignmentsListActivity extends AppCompatActivityExtended {
         loadFiles();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     private void loadFiles() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogTheme);
         builder.setView(R.layout.progress_dialog_loading).setCancelable(false);
