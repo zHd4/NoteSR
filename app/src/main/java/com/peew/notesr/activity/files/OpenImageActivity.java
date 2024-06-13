e32fc274fc8233f550948f1f7b89051c0ee705bb
zHd4 <computersystembase@gmail.com>
2024-06-14 01:09:14 +0200

    Update OpenImageActivity

--- app/src/main/java/com/peew/notesr/activity/files/OpenImageActivity.java
+++ app/src/main/java/com/peew/notesr/activity/files/OpenImageActivity.java
@@ -2,12 +2,13 @@ package com.peew.notesr.activity.files;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
 
 import com.peew.notesr.R;
+import com.peew.notesr.activity.AppCompatActivityExtended;
 import com.peew.notesr.model.File;
 
-public class OpenImageActivity extends AppCompatActivity {
+public class OpenImageActivity extends AppCompatActivityExtended {
     private File imageFile;
 
     @Override
@@ -20,5 +21,11 @@ public class OpenImageActivity extends AppCompatActivity {
         if (imageFile == null) {
             throw new RuntimeException("Image file not provided");
         }
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle(imageFile.getName());
     }
 }
