9357e1f767ed473372908c6bd90ce77adb194636
zHd4 <computersystembase@gmail.com>
2024-06-14 01:55:18 +0200

    Create FileViewerActivityBase

---
+++ app/src/main/java/com/peew/notesr/activity/files/viewer/FileViewerActivityBase.java
@@ -0,0 +1,6 @@
+package com.peew.notesr.activity.files.viewer;
+
+import com.peew.notesr.activity.AppCompatActivityExtended;
+
+public class FileViewerActivityBase extends AppCompatActivityExtended {
+}
