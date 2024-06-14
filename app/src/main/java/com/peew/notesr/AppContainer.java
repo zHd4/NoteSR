66ea71e854ce12ae96426c8b6e24a201ad7512da
zHd4 <computersystembase@gmail.com>
2024-06-15 00:41:34 +0200

    Update AppContainer

--- app/src/main/java/com/peew/notesr/AppContainer.java
+++ app/src/main/java/com/peew/notesr/AppContainer.java
@@ -1,11 +1,13 @@
 package com.peew.notesr;
 
+import com.peew.notesr.component.AssignmentsManager;
 import com.peew.notesr.crypto.CryptoManager;
 import com.peew.notesr.db.notes.NotesDatabase;
 
 public class AppContainer {
     private final NotesDatabase notesDatabase = new NotesDatabase();
     private final CryptoManager cryptoManager = new CryptoManager();
+    private final AssignmentsManager assignmentsManager = new AssignmentsManager();
 
     public NotesDatabase getNotesDatabase() {
         return notesDatabase;
@@ -13,5 +15,9 @@ public class AppContainer {
 
     public CryptoManager getCryptoManager() {
         return cryptoManager;
+    }
+
+    public AssignmentsManager getAssignmentsManager() {
+        return assignmentsManager;
     }
 }
