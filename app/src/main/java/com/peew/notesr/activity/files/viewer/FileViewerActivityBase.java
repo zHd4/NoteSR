9aea5f02f9aed545b3bc6b71ffeeb57a9ddcdbdb
zHd4 <computersystembase@gmail.com>
2024-06-20 18:14:43 +0200

    Update FileViewerActivityBase

--- app/src/main/java/com/peew/notesr/activity/files/viewer/FileViewerActivityBase.java
+++ app/src/main/java/com/peew/notesr/activity/files/viewer/FileViewerActivityBase.java
@@ -1,10 +1,7 @@
 package com.peew.notesr.activity.files.viewer;
 
-import android.content.Intent;
-import android.view.MenuItem;
 import com.peew.notesr.App;
 import com.peew.notesr.activity.AppCompatActivityExtended;
-import com.peew.notesr.activity.files.AssignmentsListActivity;
 import com.peew.notesr.component.AssignmentsManager;
 import com.peew.notesr.crypto.FilesCrypt;
 import com.peew.notesr.model.File;
@@ -14,22 +11,6 @@ import java.io.IOException;
 
 public class FileViewerActivityBase extends AppCompatActivityExtended {
     protected File file;
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            Intent intent = new Intent(App.getContext(), AssignmentsListActivity.class);
-
-            intent.putExtra("note_id", file.getNoteId());
-            startActivity(intent);
-
-            finish();
-
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     protected void loadFile() {
         //noinspection deprecation
