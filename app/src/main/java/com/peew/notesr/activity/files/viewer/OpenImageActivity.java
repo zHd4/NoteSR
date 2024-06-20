87cbd03c65ae22a6c2ed50ea0eeebeea71706d22
zHd4 <computersystembase@gmail.com>
2024-06-20 21:45:38 +0200

    Update OpenImageActivity

--- app/src/main/java/com/peew/notesr/activity/files/viewer/OpenImageActivity.java
+++ app/src/main/java/com/peew/notesr/activity/files/viewer/OpenImageActivity.java
@@ -3,15 +3,24 @@ package com.peew.notesr.activity.files.viewer;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.widget.ImageView;
 import androidx.appcompat.app.ActionBar;
 import com.peew.notesr.R;
 
 public class OpenImageActivity extends FileViewerActivityBase {
+    private ScaleGestureDetector scaleGestureDetector;
+    private float scaleFactor = 1.0f;
+    private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_image);
+
+        imageView = findViewById(R.id.assigned_image_view);
+        scaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
 
         loadFile();
 
@@ -24,12 +33,27 @@ public class OpenImageActivity extends FileViewerActivityBase {
         setImage();
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        scaleGestureDetector.onTouchEvent(motionEvent);
+        return true;
+    }
+
     private void setImage() {
         byte[] imageBytes = file.getData();
-
-        ImageView imageView = findViewById(R.id.assigned_image_view);
         Bitmap bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
 
         imageView.setImageBitmap(bitmap);
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+            scaleFactor *= scaleGestureDetector.getScaleFactor();
+            scaleFactor = Math.max(0.1f, Math.min(scaleFactor, 10.0f));
+            imageView.setScaleX(scaleFactor);
+            imageView.setScaleY(scaleFactor);
+            return true;
+        }
     }
 }
