f5725bd4eaf7d121acf5d989aa53a635024e4ef8
zHd4 <computersystembase@gmail.com>
2024-06-20 18:05:24 +0200

    Update OpenImageActivity, FileViewerActivityBase

--- app/src/main/java/com/peew/notesr/activity/files/viewer/FileViewerActivityBase.java
+++ app/src/main/java/com/peew/notesr/activity/files/viewer/FileViewerActivityBase.java
@@ -5,7 +5,12 @@ import android.view.MenuItem;
 import com.peew.notesr.App;
 import com.peew.notesr.activity.AppCompatActivityExtended;
 import com.peew.notesr.activity.files.AssignmentsListActivity;
+import com.peew.notesr.component.AssignmentsManager;
+import com.peew.notesr.crypto.FilesCrypt;
 import com.peew.notesr.model.File;
+import com.peew.notesr.model.FileInfo;
+
+import java.io.IOException;
 
 public class FileViewerActivityBase extends AppCompatActivityExtended {
     protected File file;
@@ -24,5 +29,24 @@ public class FileViewerActivityBase extends AppCompatActivityExtended {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void loadFile() {
+        //noinspection deprecation
+        FileInfo fileInfo = (FileInfo) getIntent().getSerializableExtra("file_info");
+
+        if (fileInfo == null) {
+            throw new RuntimeException("File info not provided");
+        }
+
+        try {
+            AssignmentsManager manager = App.getAppContainer().getAssignmentsManager();
+            byte[] fileData = FilesCrypt.decryptData(manager.get(fileInfo.getId()));
+
+            file = new File(fileInfo);
+            file.setData(fileData);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- app/src/main/java/com/peew/notesr/activity/files/viewer/OpenImageActivity.java
+++ app/src/main/java/com/peew/notesr/activity/files/viewer/OpenImageActivity.java
@@ -5,13 +5,7 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.ImageView;
 import androidx.appcompat.app.ActionBar;
-import com.peew.notesr.App;
 import com.peew.notesr.R;
-import com.peew.notesr.component.AssignmentsManager;
-import com.peew.notesr.model.File;
-import com.peew.notesr.model.FileInfo;
-
-import java.io.IOException;
 
 public class OpenImageActivity extends FileViewerActivityBase {
     @Override
@@ -19,21 +13,7 @@ public class OpenImageActivity extends FileViewerActivityBase {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_image);
 
-        //noinspection deprecation
-        FileInfo imageInfo = (FileInfo) getIntent().getSerializableExtra("file_info");
-
-        if (imageInfo == null) {
-            throw new RuntimeException("Image info not provided");
-        }
-
-        try {
-            AssignmentsManager manager = App.getAppContainer().getAssignmentsManager();
-
-            file = new File(imageInfo);
-            file.setData(manager.get(imageInfo.getId()));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        loadFile();
 
         ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
