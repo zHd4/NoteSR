a49e836dd77e9a4179b7ec747f4a955acaa403f6
zHd4 <computersystembase@gmail.com>
2024-06-26 01:20:22 +0200

    Update DataBlocksTable

--- app/src/main/java/com/peew/notesr/db/notes/tables/DataBlocksTable.java
+++ app/src/main/java/com/peew/notesr/db/notes/tables/DataBlocksTable.java
@@ -1,6 +1,10 @@
 package com.peew.notesr.db.notes.tables;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import com.peew.notesr.model.DataBlock;
 
 public class DataBlocksTable extends Table {
     public DataBlocksTable(SQLiteOpenHelper helper, String name, FilesTable filesTable) {
@@ -14,5 +18,53 @@ public class DataBlocksTable extends Table {
                         "data blob NOT NULL, " +
                         "FOREIGN KEY(file_id) REFERENCES " + filesTable.getName() + "(id))"
         );
+    }
+
+    public void save(DataBlock dataBlock) {
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put("file_id", dataBlock.getFileId());
+        values.put("block_order", dataBlock.getOrder());
+        values.put("data", dataBlock.getData());
+
+        if (dataBlock.getId() == null || get(dataBlock.getId()) == null) {
+            long id = db.insert(name, null, values);
+
+            if (id == -1) {
+                throw new RuntimeException("Cannot insert data block in table '" + name + "'");
+            }
+
+            dataBlock.setId(id);
+        } else {
+            db.update(name, values, "id = ?",
+                    new String[] {String.valueOf(dataBlock.getId())});
+        }
+    }
+
+    public DataBlock get(long id) {
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(
+                "SELECT " +
+                        "file_id, " +
+                        "block_order, " +
+                        "data " +
+                        " FROM " + name +
+                        " WHERE id = ?",
+                new String[] { String.valueOf(id) });
+
+        try (cursor) {
+            if (cursor.moveToFirst()) {
+                Long fileId = cursor.getLong(0);
+                Long order = cursor.getLong(1);
+
+                byte[] data = cursor.getBlob(2);
+
+                return new DataBlock(id, fileId, order, data);
+            }
+        }
+
+        return null;
     }
 }
