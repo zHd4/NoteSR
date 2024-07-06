05c0a82a86884553c55a88c176a8f931b3745898
zHd4 <computersystembase@gmail.com>
2024-07-06 02:36:29 +0200

    ServicesDatabase -> ServicesDB

--- app/src/main/java/com/peew/notesr/db/services/ServicesDB.java
+++ app/src/main/java/com/peew/notesr/db/services/ServicesDB.java
@@ -2,10 +2,10 @@ package com.peew.notesr.db.services;
 
 import com.peew.notesr.db.BaseDB;
 
-public class ServicesDatabase extends BaseDB {
+public class ServicesDB extends BaseDB {
     private static final String NAME = "services_db5";
 
-    public ServicesDatabase() {
+    public ServicesDB() {
         super(NAME);
     }
 }
