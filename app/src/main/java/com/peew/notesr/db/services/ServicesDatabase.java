5d5cf95ecb73625769db72d83ae532b5a208d2d2
zHd4 <computersystembase@gmail.com>
2024-07-06 02:35:31 +0200

    Create ServicesDatabase

---
+++ app/src/main/java/com/peew/notesr/db/services/ServicesDatabase.java
@@ -0,0 +1,11 @@
+package com.peew.notesr.db.services;
+
+import com.peew.notesr.db.BaseDB;
+
+public class ServicesDatabase extends BaseDB {
+    private static final String NAME = "services_db5";
+
+    public ServicesDatabase() {
+        super(NAME);
+    }
+}
