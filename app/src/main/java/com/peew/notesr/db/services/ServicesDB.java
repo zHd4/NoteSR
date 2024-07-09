3b4b6ef453f550ed7d581db6a95957e3fca6f653
zHd4 <computersystembase@gmail.com>
2024-07-09 03:50:18 +0200

    Update ServicesDB

--- app/src/main/java/com/peew/notesr/db/services/ServicesDB.java
+++ app/src/main/java/com/peew/notesr/db/services/ServicesDB.java
@@ -1,11 +1,14 @@
 package com.peew.notesr.db.services;
 
 import com.peew.notesr.db.BaseDB;
+import com.peew.notesr.db.services.tables.TempFilesTable;
 
 public class ServicesDB extends BaseDB {
     private static final String NAME = "services_db5";
 
     public ServicesDB() {
         super(NAME);
+
+        tables.put(TempFilesTable.class, new TempFilesTable(this, "temp_files"));
     }
 }
