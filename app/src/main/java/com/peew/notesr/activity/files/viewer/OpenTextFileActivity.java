1f5bbb01da98908c3944e465f31e04a7713c9958
zHd4 <computersystembase@gmail.com>
2024-07-13 03:40:18 +0200

    Update OpenTextFileActivity

--- app/src/main/java/com/peew/notesr/activity/files/viewer/OpenTextFileActivity.java
+++ app/src/main/java/com/peew/notesr/activity/files/viewer/OpenTextFileActivity.java
@@ -1,7 +1,11 @@
 package com.peew.notesr.activity.files.viewer;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.widget.EditText;
+import com.peew.notesr.App;
 import com.peew.notesr.R;
+import com.peew.notesr.manager.AssignmentsManager;
 
 public class OpenTextFileActivity extends FileViewerActivityBase {
 
@@ -9,5 +13,17 @@ public class OpenTextFileActivity extends FileViewerActivityBase {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_text_file);
+
+        saveDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        loadText();
+    }
+
+    private void loadText() {
+        AssignmentsManager assignmentsManager = App.getAppContainer().getAssignmentsManager();
+        EditText field = findViewById(R.id.text_file_field);
+
+        String content = new String(assignmentsManager.read(fileInfo.getId()));
+
+        field.setText(content);
     }
 }
