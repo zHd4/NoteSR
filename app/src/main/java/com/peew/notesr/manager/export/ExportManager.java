a27c8018799010996121fc36cd9e957091886948
zHd4 <computersystembase@gmail.com>
2024-07-17 05:53:13 +0200

    Update ExportManager

--- app/src/main/java/com/peew/notesr/manager/export/ExportManager.java
+++ app/src/main/java/com/peew/notesr/manager/export/ExportManager.java
@@ -2,16 +2,25 @@ package com.peew.notesr.manager.export;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
+import android.os.Environment;
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.peew.notesr.App;
+import com.peew.notesr.crypto.BackupsCrypt;
 import com.peew.notesr.manager.BaseManager;
+import com.peew.notesr.tools.FileWiper;
 import com.peew.notesr.tools.VersionFetcher;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class ExportManager extends BaseManager {
 
@@ -49,6 +58,32 @@ public class ExportManager extends BaseManager {
 
             jsonGenerator.writeEndObject();
         }
+    }
+
+    private void encrypt(File jsonFile, File outputFile) throws IOException {
+        BackupsCrypt backupsCrypt = new BackupsCrypt(jsonFile, outputFile);
+        backupsCrypt.encrypt();
+    }
+
+    private void wipe(File file) throws IOException {
+        FileWiper fileWiper = new FileWiper(file);
+        boolean success = fileWiper.wipeFile();
+
+        if (!success) {
+            throw new RuntimeException("Filed to wipe file");
+        }
+    }
+
+    private File getDumpFile() {
+        LocalDateTime now = LocalDateTime.now();
+
+        String nowStr = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        String filename = "nsr_export_" + nowStr + ".notesr.bak";
+
+        File filesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        Path dumpPath = Paths.get(filesDir.toPath().toString(), filename);
+
+        return new File(dumpPath.toUri());
     }
 
     private void writeVersion(JsonGenerator jsonGenerator) throws IOException {
