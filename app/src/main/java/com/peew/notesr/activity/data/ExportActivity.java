75b2d6d9536a587a5d564f7a4442f00051187fdb
zHd4 <computersystembase@gmail.com>
2024-07-17 09:41:16 +0200

    Update ExportActivity

--- app/src/main/java/com/peew/notesr/activity/data/ExportActivity.java
+++ app/src/main/java/com/peew/notesr/activity/data/ExportActivity.java
@@ -1,9 +1,13 @@
 package com.peew.notesr.activity.data;
 
+import android.widget.TextView;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.peew.notesr.App;
 import com.peew.notesr.R;
+import com.peew.notesr.db.notes.table.FilesInfoTable;
+import com.peew.notesr.db.notes.table.NotesTable;
 
 public class ExportActivity extends AppCompatActivity {
 
@@ -19,5 +23,22 @@ public class ExportActivity extends AppCompatActivity {
 
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle(getString(R.string.export));
+
+        TextView notesCountLabel = findViewById(R.id.notes_count_label);
+        TextView filesCountLabel = findViewById(R.id.files_count_label);
+
+        long notesCount = getNotesCount();
+        long filesCount = getFilesCount();
+
+        notesCountLabel.setText(String.format(getString(R.string.d_notes), notesCount));
+        filesCountLabel.setText(String.format(getString(R.string.d_files), filesCount));
+    }
+
+    private long getNotesCount() {
+        return App.getAppContainer().getNotesDB().getTable(NotesTable.class).getRowsCount();
+    }
+
+    private long getFilesCount() {
+        return App.getAppContainer().getNotesDB().getTable(FilesInfoTable.class).getRowsCount();
     }
 }
