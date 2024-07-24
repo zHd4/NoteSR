58dba95db36c8dd983b61984fde8866ab0ce5e8b
zHd4 <computersystembase@gmail.com>
2024-07-24 21:45:29 +0200

    Update NotesImporter

--- app/src/main/java/com/peew/notesr/manager/importer/NotesImporter.java
+++ app/src/main/java/com/peew/notesr/manager/importer/NotesImporter.java
@@ -1,9 +1,15 @@
 package com.peew.notesr.manager.importer;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.peew.notesr.crypto.NotesCrypt;
 import com.peew.notesr.db.notes.table.NotesTable;
+import com.peew.notesr.model.EncryptedNote;
+import com.peew.notesr.model.Note;
 import com.peew.notesr.tools.ProgressCounter;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 class NotesImporter {
@@ -23,7 +29,37 @@ class NotesImporter {
         this.timestampFormatter = timestampFormatter;
     }
 
-    public void importNotes() {
+    public void importNotes() throws IOException {
+        while (parser.nextToken() != JsonToken.END_ARRAY) {
+            while (parser.nextToken() != JsonToken.END_OBJECT) {
+                String field = parser.getCurrentName();
 
+                Long id = null;
+                String name = null;
+                String text = null;
+                LocalDateTime updatedAt = null;
+
+                switch (field) {
+                    case "id":
+                        id = parser.getLongValue();
+                        break;
+                    case "name":
+                        name = parser.getText();
+                        break;
+                    case "text":
+                        text = parser.getText();
+                        break;
+                    case "updated_at":
+                        updatedAt = LocalDateTime.parse(parser.getText(), timestampFormatter);
+                    default:
+                        break;
+                }
+
+                Note note = new Note(name, text, updatedAt);
+                EncryptedNote encryptedNote = NotesCrypt.encrypt(note);
+
+                notesTable.save(encryptedNote);
+            }
+        }
     }
 }
