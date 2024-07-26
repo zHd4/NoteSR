cdcb25f8ed06a472f14c88a3d2dcdbcdd658fd35
zHd4 <computersystembase@gmail.com>
2024-07-26 22:00:37 +0200

    Update ImportActivity

--- app/src/main/java/com/peew/notesr/activity/data/ImportActivity.java
+++ app/src/main/java/com/peew/notesr/activity/data/ImportActivity.java
@@ -1,14 +1,20 @@
 package com.peew.notesr.activity.data;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.ActionBar;
 import com.peew.notesr.R;
+import com.peew.notesr.activity.ExtendedAppCompatActivity;
 
-public class ImportActivity extends AppCompatActivity {
+public class ImportActivity extends ExtendedAppCompatActivity {
+
+    private ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_import);
+
+        actionBar = getSupportActionBar();
+        assert actionBar != null;
     }
 }
