10817599acd6cd9825ebb3019fa8ad9d8363d74d
zHd4 <computersystembase@gmail.com>
2024-07-26 21:59:01 +0200

    Create ImportActivity

---
+++ app/src/main/java/com/peew/notesr/activity/data/ImportActivity.java
@@ -0,0 +1,14 @@
+package com.peew.notesr.activity.data;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.peew.notesr.R;
+
+public class ImportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_import);
+    }
+}
