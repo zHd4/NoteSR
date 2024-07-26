74b9216f082651ad8365b55c15dae18d06a708f4
zHd4 <computersystembase@gmail.com>
2024-07-26 23:13:09 +0200

    Remove NotesDatabaseDump.java

--- app/src/main/java/com/peew/notesr/model/NotesDatabaseDump.java
+++
@@ -1,9 +0,0 @@
-package com.peew.notesr.model;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.util.List;
-
-public record NotesDatabaseDump(@JsonProperty("version") String version,
-                                @JsonProperty("notes") List<Note> notes) {
-}
