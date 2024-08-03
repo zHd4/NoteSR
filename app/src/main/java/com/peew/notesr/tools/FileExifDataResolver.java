2e1227f67f3939a402bac00a6ae659c7dc8591a1
zHd4 <computersystembase@gmail.com>
2024-08-03 20:14:40 +0200

    Create FileExifDataResolver

---
+++ app/src/main/java/com/peew/notesr/tools/FileExifDataResolver.java
@@ -0,0 +1,61 @@
+package com.peew.notesr.tools;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.OpenableColumns;
+import android.webkit.MimeTypeMap;
+import com.peew.notesr.App;
+
+public class FileExifDataResolver {
+
+    private final Cursor cursor;
+
+    public FileExifDataResolver(Uri uri) {
+        this.cursor = getCursor(uri);
+    }
+
+    public String getFileName() {
+        try (cursor) {
+            int index = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+
+            cursor.moveToFirst();
+            return cursor.getString(index);
+        }
+    }
+
+    public long getFileSize() {
+        try (cursor) {
+            int index = cursor.getColumnIndex(OpenableColumns.SIZE);
+
+            cursor.moveToFirst();
+            return cursor.getLong(index);
+        }
+    }
+
+    public String getMimeType() {
+        return getMimeType(getFileName());
+    }
+
+    private Cursor getCursor(Uri uri) {
+        Cursor cursor = App.getContext()
+                .getContentResolver()
+                .query(uri, null, null, null, null);
+
+        if (cursor == null) {
+            throw new RuntimeException(new NullPointerException("Cursor is null"));
+        }
+
+        return cursor;
+    }
+
+    private static String getMimeType(String filename) {
+        String type = null;
+        String extension = FileManager.getFileExtension(filename);
+
+        if (extension != null) {
+            type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+        }
+
+        return type;
+    }
+}
