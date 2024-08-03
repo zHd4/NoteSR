bb88d19dade5aaf6f0a1b78accf2f75b577b814d
zHd4 <computersystembase@gmail.com>
2024-08-03 19:46:48 +0200

    Update ImportActivity

--- app/src/main/java/com/peew/notesr/activity/data/ImportActivity.java
+++ app/src/main/java/com/peew/notesr/activity/data/ImportActivity.java
@@ -1,6 +1,13 @@
 package com.peew.notesr.activity.data;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.ActionBar;
 import com.peew.notesr.R;
 import com.peew.notesr.activity.ExtendedAppCompatActivity;
@@ -16,5 +23,33 @@ public class ImportActivity extends ExtendedAppCompatActivity {
 
         actionBar = getSupportActionBar();
         assert actionBar != null;
+    }
+
+    private View.OnClickListener selectFileOnClick() {
+        return view -> {
+            ActivityResultLauncher<Intent> resultLauncher = registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    selectFileCallback());
+
+            Intent intent = new Intent()
+                    .setType("application/x-trash")
+                    .setAction(Intent.ACTION_GET_CONTENT);
+
+            resultLauncher.launch(Intent.createChooser(intent, getString(R.string.select_a_dump)));
+        };
+    }
+
+    private ActivityResultCallback<ActivityResult> selectFileCallback() {
+        return result -> {
+            int resultCode = result.getResultCode();
+
+            if (resultCode == Activity.RESULT_OK) {
+                if (result.getData() != null) {
+
+                } else {
+                    throw new RuntimeException("Activity result is 'OK', but data not provided");
+                }
+            }
+        };
     }
 }
