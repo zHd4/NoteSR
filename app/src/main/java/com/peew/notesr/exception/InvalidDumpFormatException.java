10adf68aa44cb08405cab031ec1ec2f2c1053155
zHd4 <computersystembase@gmail.com>
2024-08-05 22:45:10 +0200

    Create InvalidDumpFormatException

---
+++ app/src/main/java/com/peew/notesr/exception/InvalidDumpFormatException.java
@@ -0,0 +1,7 @@
+package com.peew.notesr.exception;
+
+public class InvalidDumpFormatException extends Exception {
+    public InvalidDumpFormatException(String message) {
+        super(message);
+    }
+}
