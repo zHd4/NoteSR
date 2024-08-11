bd39cd1b3048ecfdc92bd09825551ea62856c019
zHd4 <computersystembase@gmail.com>
2024-08-11 07:38:51 +0200

    Update InvalidDumpFormatException

--- app/src/main/java/com/peew/notesr/exception/InvalidDumpFormatException.java
+++ app/src/main/java/com/peew/notesr/exception/InvalidDumpFormatException.java
@@ -1,13 +1,5 @@
 package com.peew.notesr.exception;
 
-public class InvalidDumpFormatException extends RuntimeException {
+public class InvalidDumpFormatException extends Exception {
     public InvalidDumpFormatException() {}
-
-    public InvalidDumpFormatException(String message) {
-        super(message);
-    }
-
-    public InvalidDumpFormatException(Exception e) {
-        super(e);
-    }
 }
