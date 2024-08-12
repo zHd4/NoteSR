f0d2cab6184c2ffafbe1d9f396399af8b916b6f7
zHd4 <computersystembase@gmail.com>
2024-08-12 02:19:09 +0200

    Update ImportResult

--- app/src/main/java/com/peew/notesr/manager/importer/ImportResult.java
+++ app/src/main/java/com/peew/notesr/manager/importer/ImportResult.java
@@ -1,5 +1,14 @@
 package com.peew.notesr.manager.importer;
 
 public enum ImportResult {
-    NONE, FINISHED_SUCCESSFULLY, DECRYPTION_FAILED, IMPORT_FAILED
+    NONE("none"),
+    FINISHED_SUCCESSFULLY("finished_successfully"),
+    DECRYPTION_FAILED("decryption_failed"),
+    IMPORT_FAILED("import_failed");
+
+    public final String result;
+
+    ImportResult(String result) {
+        this.result = result;
+    }
 }
