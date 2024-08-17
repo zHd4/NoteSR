b6c21c566ccd6fcddf4664d5075b553e1c6ed637
zHd4 <computersystembase@gmail.com>
2024-08-17 06:57:38 +0200

    Update ImportFailedException

--- app/src/main/java/com/peew/notesr/exception/ImportFailedException.java
+++ app/src/main/java/com/peew/notesr/exception/ImportFailedException.java
@@ -2,4 +2,7 @@ package com.peew.notesr.exception;
 
 public class ImportFailedException extends Exception {
     public ImportFailedException() {}
+    public ImportFailedException(Exception e) {
+        super(e);
+    }
 }
