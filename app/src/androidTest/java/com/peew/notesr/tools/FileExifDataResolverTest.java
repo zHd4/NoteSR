8237b0fd61a81bd233db4a5ebc51ebf21a340ae6
zHd4 <computersystembase@gmail.com>
2024-09-02 21:32:50 +0200

    Update FileExifDataResolverTest

--- app/src/androidTest/java/com/peew/notesr/tools/FileExifDataResolverTest.java
+++ app/src/androidTest/java/com/peew/notesr/tools/FileExifDataResolverTest.java
@@ -1,15 +1,19 @@
 package com.peew.notesr.tools;
 
+import android.net.Uri;
 import com.peew.notesr.App;
 import org.junit.BeforeClass;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Random;
 
 public class FileExifDataResolverTest {
+
+    private static final int MAX_FILE_SIZE = 100000;
 
     private static final Map<String, String> TYPES = Map.of(
             "txt", "text",
@@ -20,6 +24,7 @@ public class FileExifDataResolverTest {
     private static final Random random = new Random();
 
     private static File testFile;
+    private static FileExifDataResolver resolver;
 
     @BeforeClass
     public static void beforeAll() throws IOException {
@@ -27,5 +32,14 @@ public class FileExifDataResolverTest {
         String extension = new ArrayList<>(TYPES.keySet()).get(random.nextInt(TYPES.size()));
 
         testFile = File.createTempFile("test", "." + extension, cacheDir);
+
+        byte[] testFileData = new byte[random.nextInt(MAX_FILE_SIZE)];
+        random.nextBytes(testFileData);
+
+        try (FileOutputStream outputStream = new FileOutputStream(testFile)) {
+            outputStream.write(testFileData);
+        }
+
+        resolver = new FileExifDataResolver(Uri.fromFile(testFile));
     }
 }
