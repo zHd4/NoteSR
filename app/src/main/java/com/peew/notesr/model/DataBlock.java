da7fc3958f1796d483dd23bba33453ed679b3593
zHd4 <computersystembase@gmail.com>
2024-10-23 15:19:56 +0200

    Update DataBlock

--- app/src/main/java/com/peew/notesr/model/DataBlock.java
+++ app/src/main/java/com/peew/notesr/model/DataBlock.java
@@ -1,55 +1,27 @@
 package com.peew.notesr.model;
 
+import org.jetbrains.annotations.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Setter
+@Getter
 public class DataBlock {
     private Long id;
+
+    @NotNull
     private Long fileId;
+
+    @NotNull
     private Long order;
+
+    @NotNull
     private byte[] data;
-
-    public DataBlock() {}
-
-    public DataBlock(Long id, Long fileId, Long order, byte[] data) {
-        this.id = id;
-        this.fileId = fileId;
-        this.order = order;
-        this.data = data;
-    }
-
-    public DataBlock(Long fileId, Long order, byte[] data) {
-        this.fileId = fileId;
-        this.order = order;
-        this.data = data;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Long getFileId() {
-        return fileId;
-    }
-
-    public Long getOrder() {
-        return order;
-    }
-
-    public byte[] getData() {
-        return data;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setFileId(Long fileId) {
-        this.fileId = fileId;
-    }
-
-    public void setOrder(Long order) {
-        this.order = order;
-    }
-
-    public void setData(byte[] data) {
-        this.data = data;
-    }
 }
