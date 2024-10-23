645482841e964a2898b72c07c5edca7a0869fece
zHd4 <computersystembase@gmail.com>
2024-10-23 17:41:58 +0200

    Update FileInfo

--- app/src/main/java/com/peew/notesr/model/FileInfo.java
+++ app/src/main/java/com/peew/notesr/model/FileInfo.java
@@ -1,36 +1,23 @@
 package com.peew.notesr.model;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@RequiredArgsConstructor
 @Setter
 @Getter
 public class FileInfo implements Serializable {
     private Long id;
-
-    @NotNull
     private Long noteId;
-
-    @NotNull
     private Long size;
-
-    @NotNull
     private String name;
-
-    @NotNull
     private String type;
-
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 }
