bfcc967251b05c36ff0f1e6247587182f03c842b
zHd4 <computersystembase@gmail.com>
2024-10-23 15:21:02 +0200

    Update EncryptedFileInfo

--- app/src/main/java/com/peew/notesr/model/EncryptedFileInfo.java
+++ app/src/main/java/com/peew/notesr/model/EncryptedFileInfo.java
@@ -2,6 +2,13 @@ package com.peew.notesr.model;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
 public class EncryptedFileInfo {
     private Long id;
     private Long noteId;
@@ -10,76 +17,4 @@ public class EncryptedFileInfo {
     private byte[] encryptedType;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-
-    public EncryptedFileInfo(Long id,
-                             Long noteId,
-                             Long size,
-                             byte[] encryptedName,
-                             byte[] encryptedType,
-                             LocalDateTime createdAt,
-                             LocalDateTime updatedAt) {
-        this.id = id;
-        this.noteId = noteId;
-        this.size = size;
-        this.encryptedName = encryptedName;
-        this.encryptedType = encryptedType;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getNoteId() {
-        return noteId;
-    }
-
-    public void setNoteId(Long noteId) {
-        this.noteId = noteId;
-    }
-
-    public Long getSize() {
-        return size;
-    }
-
-    public void setSize(Long size) {
-        this.size = size;
-    }
-
-    public byte[] getEncryptedName() {
-        return encryptedName;
-    }
-
-    public void setEncryptedName(byte[] encryptedName) {
-        this.encryptedName = encryptedName;
-    }
-
-    public byte[] getEncryptedType() {
-        return encryptedType;
-    }
-
-    public void setEncryptedType(byte[] encryptedType) {
-        this.encryptedType = encryptedType;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
