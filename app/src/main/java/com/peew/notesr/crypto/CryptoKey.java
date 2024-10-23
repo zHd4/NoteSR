4c73183e4336528907cd297cd334bfe500385889
zHd4 <computersystembase@gmail.com>
2024-10-23 15:57:36 +0200

    Update CryptoKey

--- app/src/main/java/com/peew/notesr/crypto/CryptoKey.java
+++ app/src/main/java/com/peew/notesr/crypto/CryptoKey.java
@@ -1,30 +1,19 @@
 package com.peew.notesr.crypto;
 
-import java.util.Arrays;
-import java.util.Objects;
-
 import javax.crypto.SecretKey;
 
-public record CryptoKey(SecretKey key, byte[] salt, String password) {
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode
+public class CryptoKey {
 
-        CryptoKey cryptoKey = (CryptoKey) o;
-
-        return key.equals(cryptoKey.key) && Arrays.equals(salt, cryptoKey.salt) &&
-                password.equals(cryptoKey.password);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Objects.hash(key, password);
-        result = 31 * result + Arrays.hashCode(salt);
-
-        return result;
-    }
+    private SecretKey key;
+    private byte[] salt;
+    private String password;
 
     public CryptoKey copy() {
         return new CryptoKey(key, salt, password);
