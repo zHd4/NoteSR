b1ed547d2f861705f3ba9956ddef37951413bfde
zHd4 <computersystembase@gmail.com>
2024-10-23 15:29:15 +0200

    Update TempFile

--- app/src/main/java/com/peew/notesr/model/TempFile.java
+++ app/src/main/java/com/peew/notesr/model/TempFile.java
@@ -2,44 +2,18 @@ package com.peew.notesr.model;
 
 import android.net.Uri;
 
-import java.util.Objects;
+import org.jetbrains.annotations.NotNull;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
 public class TempFile {
-
     private Long id;
+
+    @NotNull
     private Uri uri;
-
-    public TempFile(Long id, Uri uri) {
-        this.id = id;
-        this.uri = uri;
-    }
-
-    public TempFile(Uri uri) {
-        this.uri = uri;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Uri getUri() {
-        return uri;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        TempFile tempFile = (TempFile) object;
-        return Objects.equals(id, tempFile.id) && Objects.equals(uri, tempFile.uri);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, uri);
-    }
 }
