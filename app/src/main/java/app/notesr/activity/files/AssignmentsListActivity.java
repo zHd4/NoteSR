0c959734c93ddbc3d8ccefe8522b97e8b705917d
zHd4 <computersystembase@gmail.com>
2024-11-06 11:49:02 +0100

    Add note modification checking in AssignmentsListActivity

--- app/src/main/java/app/notesr/activity/files/AssignmentsListActivity.java
+++ app/src/main/java/app/notesr/activity/files/AssignmentsListActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -26,6 +27,7 @@ import java.util.concurrent.Executors;
 
 public class AssignmentsListActivity extends ExtendedAppCompatActivity {
     private Note note;
+    private boolean noteModified;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +40,7 @@ public class AssignmentsListActivity extends ExtendedAppCompatActivity {
             throw new RuntimeException("Note id didn't provided");
         }
 
+        noteModified = getIntent().getBooleanExtra("modified", false);
         note = getNotesManager().get(noteId);
 
         if (note == null) {
@@ -58,6 +61,26 @@ public class AssignmentsListActivity extends ExtendedAppCompatActivity {
 
         ListView filesListView = findViewById(R.id.filesListView);
         filesListView.setOnItemClickListener(new OpenFileOnClick(this));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            if (noteModified) {
+                Intent intent = new Intent(App.getContext(), AssignmentsListActivity.class)
+                        .putExtra("noteId", note.getId())
+                        .putExtra("modified", true)
+                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                startActivity(intent);
+            } else {
+                finish();
+            }
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void configureActionBar() {
