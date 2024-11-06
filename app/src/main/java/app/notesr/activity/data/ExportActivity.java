4ba66458ac704d4c7bb6edd7b57c7bfb5306069e
zHd4 <computersystembase@gmail.com>
2024-11-06 11:05:10 +0100

    Refactor ExportActivity

--- app/src/main/java/app/notesr/activity/data/ExportActivity.java
+++ app/src/main/java/app/notesr/activity/data/ExportActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -58,18 +57,6 @@ public class ExportActivity extends ExtendedAppCompatActivity {
 
         notesCountLabel.setText(String.format(getString(R.string.d_notes), notesCount));
         filesCountLabel.setText(String.format(getString(R.string.d_files), filesCount));
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == android.R.id.home) {
-            finish();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     private View.OnClickListener startStopButtonOnClick() {
