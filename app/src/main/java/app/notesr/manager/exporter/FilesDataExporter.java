33f175191978b4d7acc3c78361f3923531250f71
zHd4 <computersystembase@gmail.com>
2024-11-11 10:14:16 +0100

    Create and implement FilesDataExporter

---
+++ app/src/main/java/app/notesr/manager/exporter/FilesDataExporter.java
@@ -0,0 +1,34 @@
+package app.notesr.manager.exporter;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+
+import app.notesr.crypto.FilesCrypt;
+import app.notesr.db.notes.table.DataBlocksTable;
+import app.notesr.db.notes.table.FilesInfoTable;
+import app.notesr.model.DataBlock;
+import app.notesr.utils.FilesUtils;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+class FilesDataExporter {
+    private final File outputDir;
+    private final FilesInfoTable filesInfoTable;
+    private final DataBlocksTable dataBlocksTable;
+
+    public void export() throws IOException {
+        for (Long fileId : filesInfoTable.getAllIds()) {
+            Set<Long> blocksId = dataBlocksTable.getBlocksIdsByFileId(fileId);
+
+            for (Long blockId : blocksId) {
+                DataBlock block = dataBlocksTable.get(blockId);
+
+                Long id = block.getId();
+                byte[] data = FilesCrypt.decryptData(block.getData());
+
+                FilesUtils.writeFileBytes(new File(outputDir, id.toString()), data);
+            }
+        }
+    }
+}
