a92be508f7b8e89600b19197d10789f01b4bc796
zHd4 <computersystembase@gmail.com>
2024-11-11 09:52:05 +0100

    Update Writer

--- app/src/main/java/app/notesr/manager/exporter/Writer.java
+++ app/src/main/java/app/notesr/manager/exporter/Writer.java
@@ -1,6 +1,12 @@
 package app.notesr.manager.exporter;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+
+import java.io.IOException;
+
 public interface Writer {
+    void write() throws IOException;
+    JsonGenerator getJsonGenerator();
     long getTotal();
     long getExported();
 }
