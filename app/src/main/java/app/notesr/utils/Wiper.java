c6d66a9d6fe58a584f6099eb23b0504ba44a4c3a
zHd4 <computersystembase@gmail.com>
2024-11-12 14:54:35 +0100

    Refactor Wiper

--- app/src/main/java/app/notesr/utils/Wiper.java
+++ app/src/main/java/app/notesr/utils/Wiper.java
@@ -11,26 +11,20 @@ public class Wiper {
     private static final String TAG = Wiper.class.getName();
     private static final int LOOPS_COUNT = 6;
 
-    private final File file;
-
-    public Wiper(File file) {
-        this.file = file;
-    }
-
-    public boolean wipeFile() throws IOException {
-        long fileSize = file.length();
-
+    public static boolean wipeFile(File file) throws IOException {
         for (int i = 0; i < LOOPS_COUNT; i++) {
-            wipeFileData(fileSize);
+            wipeFileData(file);
         }
 
         return file.delete();
     }
 
-    private void wipeFileData(long size) throws IOException {
+    private static void wipeFileData(File file) throws IOException {
+        long fileSize = file.length();
+
         try (FileOutputStream stream = new FileOutputStream(file)) {
             try {
-                stream.write(new byte[(int) size]);
+                stream.write(new byte[(int) fileSize]);
             } catch (OutOfMemoryError e) {
                 long bytesWrite = 0;
 
@@ -44,12 +38,12 @@ public class Wiper {
                         Log.e(TAG, "OutOfMemoryError", error);
                     }
 
-                } while (bytesWrite < size);
+                } while (bytesWrite < fileSize);
             }
         }
     }
 
-    private long getAvailableMemory() {
+    private static long getAvailableMemory() {
         Runtime runtime = Runtime.getRuntime();
         return runtime.maxMemory() - (runtime.totalMemory() - runtime.freeMemory());
     }
