da14a2614d25acaf8cb9f4da2c55ec2876f80f87
zHd4 <computersystembase@gmail.com>
2024-11-15 09:10:08 +0100

    Update FilesDataExporter

--- app/src/main/java/app/notesr/manager/exporter/FilesDataExporter.java
+++ app/src/main/java/app/notesr/manager/exporter/FilesDataExporter.java
@@ -2,11 +2,9 @@ package app.notesr.manager.exporter;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Set;
 
 import app.notesr.crypto.FilesCrypt;
 import app.notesr.db.notes.table.DataBlocksTable;
-import app.notesr.db.notes.table.FilesInfoTable;
 import app.notesr.model.DataBlock;
 import app.notesr.utils.FilesUtils;
 import lombok.AllArgsConstructor;
@@ -14,7 +12,6 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 class FilesDataExporter {
     private final File outputDir;
-    private final FilesInfoTable filesInfoTable;
     private final DataBlocksTable dataBlocksTable;
 
     public void export() throws IOException {
@@ -24,17 +21,11 @@ class FilesDataExporter {
             }
         }
 
-        for (Long fileId : filesInfoTable.getAllIds()) {
-            Set<Long> blocksId = dataBlocksTable.getBlocksIdsByFileId(fileId);
+        for (DataBlock blockWithoutData : dataBlocksTable.getAllWithoutData()) {
+            DataBlock dataBlock = dataBlocksTable.get(blockWithoutData.getId());
 
-            for (Long blockId : blocksId) {
-                DataBlock block = dataBlocksTable.get(blockId);
-
-                Long id = block.getId();
-                byte[] data = FilesCrypt.decryptData(block.getData());
-
-                FilesUtils.writeFileBytes(new File(outputDir, id.toString()), data);
-            }
+            byte[] data = FilesCrypt.decryptData(dataBlock.getData());
+            FilesUtils.writeFileBytes(new File(outputDir, dataBlock.getId().toString()), data);
         }
     }
 }
