ce8a0bbc9d866bea301a4e3a710196c60a821e66
zHd4 <computersystembase@gmail.com>
2024-11-15 09:19:26 +0100

    Update Exporter

--- app/src/main/java/app/notesr/manager/exporter/Exporter.java
+++ app/src/main/java/app/notesr/manager/exporter/Exporter.java
@@ -2,6 +2,7 @@ package app.notesr.manager.exporter;
 
 import java.io.IOException;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -10,12 +11,20 @@ import lombok.RequiredArgsConstructor;
 @Getter
 abstract class Exporter {
     @NonNull
+    @Getter(AccessLevel.PROTECTED)
     private ExportThread thread;
 
+    @Getter(AccessLevel.PACKAGE)
     private long exported;
 
     protected void increaseExported() {
         exported++;
+    }
+
+    protected void breakOnInterrupted() throws InterruptedException {
+        if (thread.isInterrupted()) {
+            throw new InterruptedException();
+        }
     }
 
     abstract void export() throws IOException, InterruptedException;
