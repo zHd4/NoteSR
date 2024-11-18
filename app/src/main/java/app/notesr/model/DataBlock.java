a136d89a3102092f0396bdd89147d074248f67ec
zHd4 <computersystembase@gmail.com>
2024-11-18 08:58:55 +0100

    Remove RequiredArgsConstructor from DataBlock

--- app/src/main/java/app/notesr/model/DataBlock.java
+++ app/src/main/java/app/notesr/model/DataBlock.java
@@ -1,29 +1,19 @@
 package app.notesr.model;
 
-import org.jetbrains.annotations.NotNull;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@RequiredArgsConstructor
 @Setter
 @Getter
 public class DataBlock {
     private Long id;
-
-    @NotNull
     private Long fileId;
-
-    @NotNull
     private Long order;
-
-    @NotNull
     private byte[] data;
 }
