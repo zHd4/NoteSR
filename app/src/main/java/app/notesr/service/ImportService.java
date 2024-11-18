5fbe3e1c0a4e95ab75f1ad028502a1559d14d66f
zHd4 <computersystembase@gmail.com>
2024-11-18 10:48:37 +0100

    Move ImportService

--- app/src/main/java/app/notesr/service/ImportService.java
+++ app/src/main/java/app/notesr/service/ImportService.java
@@ -14,8 +14,8 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-import app.notesr.manager.importer.ImportManager;
-import app.notesr.manager.importer.ImportResult;
+import app.notesr.manager.importer.v1.ImportManager;
+import app.notesr.manager.importer.v1.ImportResult;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
