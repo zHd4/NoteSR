cc05d544360b2d0299f479c3241a03730fed2a7b
zHd4 <computersystembase@gmail.com>
2024-11-20 11:04:48 +0100

    Update start in ImportManagerV2

--- app/src/main/java/app/notesr/manager/importer/v2/ImportManagerV2.java
+++ app/src/main/java/app/notesr/manager/importer/v2/ImportManagerV2.java
@@ -1,19 +1,25 @@
 package app.notesr.manager.importer.v2;
 
-import android.content.Context;
+import static java.util.UUID.randomUUID;
 
+import android.content.Context;
 import java.io.File;
+import java.io.IOException;
 
 import app.notesr.manager.importer.BaseImportManager;
 import app.notesr.manager.importer.ImportResult;
+import app.notesr.utils.ZipUtils;
 import lombok.Getter;
 
 public class ImportManagerV2 extends BaseImportManager {
+
     @Getter
     private ImportResult result = ImportResult.NONE;
 
     @Getter
     private String status = "";
+
+    private File tempDir;
 
     public ImportManagerV2(Context context, File file) {
         super(context, file);
@@ -21,6 +27,15 @@ public class ImportManagerV2 extends BaseImportManager {
 
     @Override
     public void start() {
+        Thread thread = new Thread(() -> {
+            try {
+                tempDir = new File(context.getCacheDir(), randomUUID().toString());
+                ZipUtils.unzip(file.getAbsolutePath(), tempDir.getAbsolutePath(), null);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
 
+        thread.start();
     }
 }
