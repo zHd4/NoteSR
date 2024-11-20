79b918dd661a93582dd4e52031a70234eeb9f7fe
zHd4 <computersystembase@gmail.com>
2024-11-20 09:32:18 +0100

    Update BaseImportManager

--- app/src/main/java/app/notesr/manager/importer/BaseImportManager.java
+++ app/src/main/java/app/notesr/manager/importer/BaseImportManager.java
@@ -4,7 +4,6 @@ import android.content.Context;
 
 import java.io.FileInputStream;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -12,11 +11,7 @@ public abstract class BaseImportManager {
     protected final Context context;
     protected final FileInputStream sourceStream;
 
-    @Getter
-    protected ImportResult result = ImportResult.NONE;
-
-    @Getter
-    protected String status = "";
-
     public abstract void start();
+    public abstract ImportResult getResult();
+    public abstract String getStatus();
 }
