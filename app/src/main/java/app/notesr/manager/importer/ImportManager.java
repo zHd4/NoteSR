0e6af60c925edbd12b54d06e37eb791c6c8f4196
zHd4 <computersystembase@gmail.com>
2024-11-20 09:18:36 +0100

    Implement wipeFile in ImportManager

--- app/src/main/java/app/notesr/manager/importer/ImportManager.java
+++ app/src/main/java/app/notesr/manager/importer/ImportManager.java
@@ -3,12 +3,14 @@ package app.notesr.manager.importer;
 import android.content.Context;
 import android.util.Log;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 import app.notesr.crypto.BackupsCrypt;
 import app.notesr.exception.DecryptionFailedException;
+import app.notesr.utils.Wiper;
 
 public class ImportManager extends BaseImportManager {
     private static final String TAG = ImportManager.class.getName();
@@ -30,6 +32,16 @@ public class ImportManager extends BaseImportManager {
         } catch (IOException e) {
             Log.e(TAG, "IOException", e);
             throw new DecryptionFailedException();
+        }
+    }
+
+    private static void wipeFile(File file) {
+        try {
+            if (!Wiper.wipeFile(file)) {
+                throw new RuntimeException("Cannot delete file: " + file.getAbsolutePath());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
