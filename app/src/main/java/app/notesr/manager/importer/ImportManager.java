979cf4101949703597a6596af6429526e6a96c87
zHd4 <computersystembase@gmail.com>
2024-11-20 09:18:13 +0100

    Implement decrypt in ImportManager

--- app/src/main/java/app/notesr/manager/importer/ImportManager.java
+++ app/src/main/java/app/notesr/manager/importer/ImportManager.java
@@ -1,10 +1,18 @@
 package app.notesr.manager.importer;
 
 import android.content.Context;
+import android.util.Log;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-public class ImportManager extends BaseImportManager{
+import app.notesr.crypto.BackupsCrypt;
+import app.notesr.exception.DecryptionFailedException;
+
+public class ImportManager extends BaseImportManager {
+    private static final String TAG = ImportManager.class.getName();
+
     public ImportManager(Context context, FileInputStream sourceStream) {
         super(context, sourceStream);
     }
@@ -12,5 +20,16 @@ public class ImportManager extends BaseImportManager{
     @Override
     public void start() {
 
+    }
+
+    private static void decrypt(FileInputStream inputStream, FileOutputStream outputStream) throws
+            DecryptionFailedException {
+        try {
+            BackupsCrypt backupsCrypt = new BackupsCrypt(inputStream, outputStream);
+            backupsCrypt.decrypt();
+        } catch (IOException e) {
+            Log.e(TAG, "IOException", e);
+            throw new DecryptionFailedException();
+        }
     }
 }
