6a3389a37481a72aa20c7316ebd87a55672b1b20
zHd4 <computersystembase@gmail.com>
2024-11-26 10:09:38 +0100

    Implement TempDataWiper

---
+++ app/src/main/java/app/notesr/manager/data/TempDataWiper.java
@@ -0,0 +1,28 @@
+package app.notesr.manager.data;
+
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+
+import app.notesr.utils.Wiper;
+
+public class TempDataWiper {
+    private static final String TAG = TempDataWiper.class.getName();
+
+    public static void wipeTempData(File file, File dir) {
+        try {
+            if (file != null) checkResult(Wiper.wipeFile(file));
+            if (dir != null) checkResult(Wiper.wipeDir(dir));
+        } catch (IOException e) {
+            Log.e(TAG, "IOException", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void checkResult(boolean success) {
+        if (!success) {
+            throw new IllegalStateException("Temp data has not been wiped");
+        }
+    }
+}
