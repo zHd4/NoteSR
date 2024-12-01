db3fbaaf10ba5c0d8bdf9fcbbfd79b899c138141
zHd4 <computersystembase@gmail.com>
2024-12-01 20:13:34 +0100

    Apply builder pattern to the FileInfo

--- app/src/main/java/app/notesr/model/FileInfo.java
+++ app/src/main/java/app/notesr/model/FileInfo.java
@@ -4,12 +4,14 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Setter
 @Getter
 public class FileInfo implements Serializable {
