f4d699bb7517daa968cf925582c5e9b030f1ba88
zHd4 <computersystembase@gmail.com>
2024-12-05 18:54:24 +0100

    Update getItemId in FilesListAdapter

--- app/src/main/java/app/notesr/adapter/FilesListAdapter.java
+++ app/src/main/java/app/notesr/adapter/FilesListAdapter.java
@@ -81,6 +81,6 @@ public class FilesListAdapter extends ElementsListAdapter<FileInfo> {
 
     @Override
     public long getItemId(int position) {
-        return Objects.requireNonNull(getItem(position)).getId().hashCode();
+        return Objects.requireNonNull(getItem(position)).getDecimalId();
     }
 }
