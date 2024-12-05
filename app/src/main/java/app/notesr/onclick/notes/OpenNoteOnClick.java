24dc494af4b59c7c66a8758fac2ec5fb8fae7071
zHd4 <computersystembase@gmail.com>
2024-12-05 19:18:35 +0100

    Update onItemClick in OpenNoteOnClick

--- app/src/main/java/app/notesr/onclick/notes/OpenNoteOnClick.java
+++ app/src/main/java/app/notesr/onclick/notes/OpenNoteOnClick.java
@@ -4,22 +4,24 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.AdapterView;
 
+import java.util.Map;
+
 import app.notesr.App;
 import app.notesr.activity.ExtendedAppCompatActivity;
 import app.notesr.activity.notes.OpenNoteActivity;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class OpenNoteOnClick implements AdapterView.OnItemClickListener {
     private final ExtendedAppCompatActivity activity;
-
-    public OpenNoteOnClick(ExtendedAppCompatActivity activity) {
-        this.activity = activity;
-    }
+    private final Map<Long, String> notesIdsMap;
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String noteId = notesIdsMap.get(id);
         Intent noteOpenActivtyIntent = new Intent(App.getContext(), OpenNoteActivity.class);
 
-        noteOpenActivtyIntent.putExtra("noteId", id);
+        noteOpenActivtyIntent.putExtra("noteId", noteId);
         activity.startActivity(noteOpenActivtyIntent);
     }
 }
