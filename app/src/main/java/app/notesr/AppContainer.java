08e2e15f49dc93da60ff73a77a321f0e0715aefa
zHd4 <computersystembase@gmail.com>
2024-12-06 00:36:58 +0100

    Refactor AppContainer

--- app/src/main/java/app/notesr/AppContainer.java
+++ app/src/main/java/app/notesr/AppContainer.java
@@ -6,9 +6,11 @@ import app.notesr.db.services.ServicesDB;
 import app.notesr.manager.AssignmentsManager;
 import app.notesr.manager.KeyUpdateManager;
 import app.notesr.manager.NotesManager;
+import lombok.Getter;
 
 import java.time.format.DateTimeFormatter;
 
+@Getter
 public class AppContainer {
     private final NotesDB notesDB = new NotesDB();
     private final ServicesDB servicesDB = new ServicesDB();
@@ -19,32 +21,4 @@ public class AppContainer {
     private final NotesManager notesManager = new NotesManager();
     private final AssignmentsManager assignmentsManager = new AssignmentsManager();
     private final KeyUpdateManager keyUpdateManager = new KeyUpdateManager();
-
-    public NotesDB getNotesDB() {
-        return notesDB;
-    }
-
-    public ServicesDB getServicesDB() {
-        return servicesDB;
-    }
-
-    public DateTimeFormatter getTimestampFormatter() {
-        return timestampFormatter;
-    }
-
-    public CryptoManager getCryptoManager() {
-        return cryptoManager;
-    }
-
-    public NotesManager getNotesManager() {
-        return notesManager;
-    }
-
-    public AssignmentsManager getAssignmentsManager() {
-        return assignmentsManager;
-    }
-
-    public KeyUpdateManager getKeyUpdateManager() {
-        return keyUpdateManager;
-    }
 }
