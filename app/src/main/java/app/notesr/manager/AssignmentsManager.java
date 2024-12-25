f41b3b68abed47a4d3f9603ff5542f77054a762e
zHd4 <computersystembase@gmail.com>
2024-12-25 16:19:40 +0100

    Refactor saveData in AssignmentsManager

--- app/src/main/java/app/notesr/manager/AssignmentsManager.java
+++ app/src/main/java/app/notesr/manager/AssignmentsManager.java
@@ -52,37 +52,37 @@ public class AssignmentsManager extends BaseManager {
     }
 
     public void saveData(String fileId, InputStream stream) throws IOException {
-        try (stream) {
-            DataBlocksTable dataBlocksTable = getDataBlocksTable();
+        DataBlocksTable dataBlocksTable = getDataBlocksTable();
 
-            byte[] chunk = new byte[CHUNK_SIZE];
+        byte[] chunk = new byte[CHUNK_SIZE];
 
-            long order = 0;
-            int bytesRead = stream.read(chunk);
+        long order = 0;
+        int bytesRead = stream.read(chunk);
 
-            while (bytesRead != -1) {
-                if (bytesRead != CHUNK_SIZE) {
-                    byte[] subChunk = new byte[bytesRead];
-                    System.arraycopy(chunk, 0, subChunk, 0, bytesRead);
-                    chunk = subChunk;
-                }
-
-                chunk = FilesCrypt.encryptData(chunk);
-
-                DataBlock dataBlock = DataBlock.builder()
-                        .fileId(fileId)
-                        .order(order)
-                        .data(chunk)
-                        .build();
-
-                dataBlocksTable.save(dataBlock);
-
-                chunk = new byte[CHUNK_SIZE];
-                bytesRead = stream.read(chunk);
-
-                order++;
+        while (bytesRead != -1) {
+            if (bytesRead != CHUNK_SIZE) {
+                byte[] subChunk = new byte[bytesRead];
+                System.arraycopy(chunk, 0, subChunk, 0, bytesRead);
+                chunk = subChunk;
             }
+
+            chunk = FilesCrypt.encryptData(chunk);
+
+            DataBlock dataBlock = DataBlock.builder()
+                    .fileId(fileId)
+                    .order(order)
+                    .data(chunk)
+                    .build();
+
+            dataBlocksTable.save(dataBlock);
+
+            chunk = new byte[CHUNK_SIZE];
+            bytesRead = stream.read(chunk);
+
+            order++;
         }
+
+        stream.close();
     }
 
     public byte[] read(String fileId) {
