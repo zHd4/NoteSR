df4b0c8ca1b5d7c7d2acc8a2e6e2d431190f0d86
zHd4 <computersystembase@gmail.com>
2025-01-02 21:00:41 +0100

    Refactor StartActivity

--- app/src/main/java/app/notesr/activity/StartActivity.java
+++ app/src/main/java/app/notesr/activity/StartActivity.java
@@ -13,9 +13,10 @@ import app.notesr.R;
 import app.notesr.activity.security.AuthActivity;
 
 public class StartActivity extends ExtendedAppCompatActivity {
+
     private static final double BANNER_MARGIN_FOR_LOW_HEIGHT = 0.1;
     private static final double BANNER_MARGIN_FOR_LARGE_HEIGHT = 0.2;
-    private static final int LOW_HEIGHT = 800;
+    private static final int LOW_SCREEN_HEIGHT = 800;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +39,7 @@ public class StartActivity extends ExtendedAppCompatActivity {
 
         int screenHeight = displayMetrics.heightPixels;
 
-        double percent = screenHeight > LOW_HEIGHT
+        double percent = screenHeight > LOW_SCREEN_HEIGHT
                 ? BANNER_MARGIN_FOR_LARGE_HEIGHT
                 : BANNER_MARGIN_FOR_LOW_HEIGHT;
 
