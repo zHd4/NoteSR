102b6b6f3be2fad15914392a5e69c878c7f6721b
zHd4 <computersystembase@gmail.com>
2025-01-09 15:25:02 +0100

    Create ThumbnailsTable

--- app/src/main/java/app/notesr/db/notes/NotesDB.java
+++ app/src/main/java/app/notesr/db/notes/NotesDB.java
@@ -4,6 +4,7 @@ import app.notesr.db.BaseDB;
 import app.notesr.db.notes.table.DataBlocksTable;
 import app.notesr.db.notes.table.FilesInfoTable;
 import app.notesr.db.notes.table.NotesTable;
+import app.notesr.db.notes.table.ThumbnailsTable;
 
 public class NotesDB extends BaseDB {
     private static final String NAME = "notes_db5";
@@ -14,6 +15,7 @@ public class NotesDB extends BaseDB {
         NotesTable notesTable = new NotesTable(this, "notes");
         FilesInfoTable filesInfoTable = new FilesInfoTable(this, "files_info", notesTable);
         DataBlocksTable dataBlocksTable = new DataBlocksTable(this, "data_blocks", filesInfoTable);
+        ThumbnailsTable thumbnailsTable = new ThumbnailsTable(this, "thumbnails", filesInfoTable);
 
         tables.put(NotesTable.class, notesTable);
         tables.put(FilesInfoTable.class, filesInfoTable);
---
+++ app/src/main/java/app/notesr/db/notes/table/ThumbnailsTable.java
@@ -0,0 +1,74 @@
+package app.notesr.db.notes.table;
+
+import static java.util.UUID.randomUUID;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+
+import app.notesr.db.BaseTable;
+import app.notesr.db.notes.NotesDB;
+import app.notesr.model.EncryptedFileThumbnail;
+
+public class ThumbnailsTable extends BaseTable {
+    public ThumbnailsTable(NotesDB db, String name, FilesInfoTable filesInfoTable) {
+        super(db, name);
+
+        db.writableDatabase.execSQL(
+                "CREATE TABLE IF NOT EXISTS " + name + "(" +
+                        "id varchar(36) PRIMARY KEY CHECK(LENGTH(id) = 36), " +
+                        "file_id varchar(36) NOT NULL, " +
+                        "encrypted_image blob NOT NULL, " +
+                        "FOREIGN KEY(file_id) REFERENCES " + filesInfoTable.getName() + "(id))"
+        );
+    }
+
+    public void save(EncryptedFileThumbnail thumbnail) {
+        save(thumbnail, true);
+    }
+
+    public void save(EncryptedFileThumbnail thumbnail, boolean generateId) {
+        ContentValues values = new ContentValues();
+
+        values.put("file_id", thumbnail.getFileId());
+        values.put("encrypted_image", thumbnail.getEncryptedImage());
+
+        if (thumbnail.getId() == null || get(thumbnail.getId()) == null) {
+            String id = generateId ? randomUUID().toString() : thumbnail.getId();
+            values.put("id", id);
+
+            if (db.writableDatabase.insert(name, null, values) == -1) {
+                throw new RuntimeException("Cannot insert data block in table '" + name + "'");
+            }
+
+            thumbnail.setId(id);
+        } else {
+            db.writableDatabase.update(name, values, "id = ?",
+                    new String[] {String.valueOf(thumbnail.getId())});
+        }
+    }
+
+    public EncryptedFileThumbnail get(String id) {
+        Cursor cursor = db.readableDatabase.rawQuery(
+                "SELECT " +
+                        "file_id, " +
+                        "encrypted_image " +
+                        " FROM " + name +
+                        " WHERE id = ?",
+                new String[] {id});
+
+        try (cursor) {
+            if (cursor.moveToFirst()) {
+                String fileId = cursor.getString(0);
+                byte[] encryptedImage = cursor.getBlob(1);
+
+                return EncryptedFileThumbnail.builder()
+                        .id(id)
+                        .fileId(fileId)
+                        .encryptedImage(encryptedImage)
+                        .build();
+            }
+        }
+
+        return null;
+    }
+}
