fc58ad178bee06bb249715e5b6d7a82d4401c578
zHd4 <computersystembase@gmail.com>
2025-01-10 12:07:50 +0100

    Remove ThumbnailsTable

--- app/src/main/java/app/notesr/db/notes/NotesDB.java
+++ app/src/main/java/app/notesr/db/notes/NotesDB.java
@@ -4,7 +4,6 @@ import app.notesr.db.BaseDB;
 import app.notesr.db.notes.table.DataBlocksTable;
 import app.notesr.db.notes.table.FilesInfoTable;
 import app.notesr.db.notes.table.NotesTable;
-import app.notesr.db.notes.table.ThumbnailsTable;
 
 public class NotesDB extends BaseDB {
     private static final String NAME = "notes_db5";
@@ -15,7 +14,6 @@ public class NotesDB extends BaseDB {
         NotesTable notesTable = new NotesTable(this, "notes");
         FilesInfoTable filesInfoTable = new FilesInfoTable(this, "files_info", notesTable);
         DataBlocksTable dataBlocksTable = new DataBlocksTable(this, "data_blocks", filesInfoTable);
-        ThumbnailsTable thumbnailsTable = new ThumbnailsTable(this, "thumbnails", filesInfoTable);
 
         tables.put(NotesTable.class, notesTable);
         tables.put(FilesInfoTable.class, filesInfoTable);
