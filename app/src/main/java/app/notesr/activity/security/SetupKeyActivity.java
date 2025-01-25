755ef0d8bb1568ad5cd09faa03b3e167209d8e20
zHd4 <computersystembase@gmail.com>
2025-01-25 22:08:41 +0100

    Update SetupKeyActivity

--- app/src/main/java/app/notesr/activity/security/SetupKeyActivity.java
+++ app/src/main/java/app/notesr/activity/security/SetupKeyActivity.java
@@ -1,25 +1,28 @@
 package app.notesr.activity.security;
 
-import static androidx.core.view.inputmethod.EditorInfoCompat.IME_FLAG_NO_PERSONALIZED_LEARNING;
+import static java.util.Objects.requireNonNull;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import app.notesr.App;
 import app.notesr.R;
 import app.notesr.activity.ExtendedAppCompatActivity;
-import app.notesr.model.CryptoKey;
-import app.notesr.crypto.CryptoTools;
-import app.notesr.onclick.security.FinishKeySetupOnClick;
+import app.notesr.activity.notes.NotesListActivity;
+import app.notesr.service.activity.security.KeySetupService;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
-import java.security.NoSuchAlgorithmException;
 
 @Getter
 public class SetupKeyActivity extends ExtendedAppCompatActivity {
@@ -37,30 +40,31 @@ public class SetupKeyActivity extends ExtendedAppCompatActivity {
     private static final float KEY_VIEW_TEXT_SIZE_FOR_LOW_SCREEN_HEIGHT = 16;
 
     private Mode mode;
+    private String password;
+    private KeySetupService keySetupService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_setup_key);
         setMode();
 
+        password = requireNonNull(getIntent().getStringExtra("password"));
+        keySetupService = new KeySetupService(password);
+
         TextView keyView = findViewById(R.id.aesKeyHex);
+        keyView.setText(keySetupService.getHexKey());
 
-        EditText importKeyField = findViewById(R.id.importKeyField);
-        importKeyField.setImeOptions(IME_FLAG_NO_PERSONALIZED_LEARNING);
-
-        Button copyToClipboardButton = findViewById(R.id.copyAesKeyHex);
-        Button nextButton = findViewById(R.id.keySetupNextButton);
-
-        String password = getPassword();
-        CryptoKey key = getKey(password);
-        String keyHex = CryptoTools.cryptoKeyToHex(key);
-
-        keyView.setText(keyHex);
         adaptKeyView();
 
-        copyToClipboardButton.setOnClickListener(getCopyKeyOnClick(keyHex));
-        nextButton.setOnClickListener(new FinishKeySetupOnClick(this, password, key));
+        Button copyToClipboardButton = findViewById(R.id.copyAesKeyHex);
+        Button importButton = findViewById(R.id.importHexKeyButton);
+        Button nextButton = findViewById(R.id.keySetupNextButton);
+
+        copyToClipboardButton.setOnClickListener(copyKeyButtonOnClick());
+        importButton.setOnClickListener(importKeyButtonOnClick());
+        nextButton.setOnClickListener(nextButtonOnClick());
     }
 
     private void setMode() {
@@ -81,24 +85,6 @@ public class SetupKeyActivity extends ExtendedAppCompatActivity {
         }
     }
 
-    private String getPassword() {
-        String password = getIntent().getStringExtra("password");
-
-        if (password == null) {
-            throw new RuntimeException("Password didn't provided");
-        }
-
-        return password;
-    }
-
-    private CryptoKey getKey(String password) {
-        try {
-            return App.getAppContainer().getCryptoManager().generateNewKey(password);
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     private void adaptKeyView() {
         TextView keyView = findViewById(R.id.aesKeyHex);
 
@@ -107,10 +93,62 @@ public class SetupKeyActivity extends ExtendedAppCompatActivity {
         }
     }
 
-    private View.OnClickListener getCopyKeyOnClick(String keyHex) {
+    private View.OnClickListener copyKeyButtonOnClick() {
         return view -> {
+            String keyHex = ((TextView) findViewById(R.id.aesKeyHex)).getText().toString();
+
             copyToClipboard("", keyHex);
             showToastMessage(getString(R.string.copied), Toast.LENGTH_SHORT);
         };
+    }
+
+    private View.OnClickListener importKeyButtonOnClick() {
+        return view -> {
+            Intent intent = new Intent(getApplicationContext(), NotesListActivity.class)
+                    .putExtra("password", password);
+
+            startActivity(intent);
+        };
+    }
+
+    private View.OnClickListener nextButtonOnClick() {
+        return view -> {
+            if (mode == Mode.FIRST_RUN) proceedFirstRun();
+            else if (mode == Mode.REGENERATION) proceedRegeneration();
+        };
+    }
+
+    private void proceedFirstRun() {
+        try {
+            keySetupService.apply();
+            startActivity(new Intent(getApplicationContext(), NotesListActivity.class));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void proceedRegeneration() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogTheme);
+
+        executor.execute(() -> {
+            handler.post(() -> {
+                builder.setView(R.layout.progress_dialog_re_encryption);
+                builder.setCancelable(false);
+                builder.create().show();
+            });
+
+            try {
+                App.getAppContainer()
+                        .getKeyUpdateService()
+                        .updateEncryptedData(keySetupService.getCryptoKey());
+                startActivity(new Intent(getApplicationContext(), NotesListActivity.class));
+                finish();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
