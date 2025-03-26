c91a528b565249c943431d81005c68dee8b257e9
zHd4 <computersystembase@gmail.com>
2025-03-26 17:16:41 +0100

    Refactor TempDataWiper

--- app/src/main/java/app/notesr/service/data/TempDataWiper.java
+++ app/src/main/java/app/notesr/service/data/TempDataWiper.java
@@ -1,28 +1,28 @@
 package app.notesr.service.data;
 
+import static app.notesr.util.Wiper.wipeDir;
+import static app.notesr.util.Wiper.wipeFile;
+
 import android.util.Log;
 
 import java.io.File;
 import java.io.IOException;
 
-import app.notesr.util.Wiper;
-
 public class TempDataWiper {
     private static final String TAG = TempDataWiper.class.getName();
 
-    public static void wipeTempData(File file, File dir) {
+    public static void wipeTempData(File... objects) {
         try {
-            if (file != null) checkResult(Wiper.wipeFile(file));
-            if (dir != null) checkResult(Wiper.wipeDir(dir));
+            for (File object : objects) {
+                boolean isWiped = object.isDirectory() ? wipeDir(object) : wipeFile(object);
+
+                if (!isWiped) {
+                    throw new IllegalStateException("Temp data was not wiped");
+                }
+            }
         } catch (IOException e) {
             Log.e(TAG, "IOException", e);
             throw new RuntimeException(e);
-        }
-    }
-
-    private static void checkResult(boolean success) {
-        if (!success) {
-            throw new IllegalStateException("Temp data has not been wiped");
         }
     }
 }
