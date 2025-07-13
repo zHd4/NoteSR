b1deaa3244fb5448aeeac5afc7a8d1ca1eee240b
zHd4 <computersystembase@gmail.com>
2025-07-13 16:41:18 +0200

    Create MigrationActivity

---
+++ app/src/main/java/app/notesr/activity/data/MigrationActivity.java
@@ -0,0 +1,16 @@
+package app.notesr.activity.data;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.notesr.R;
+
+public class MigrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_migration);
+    }
+}
