cd5d98c860a0c813ffdf9a3e99216a2729b15a71
zHd4 <computersystembase@gmail.com>
2025-07-14 18:37:17 +0200

    Implement app migration in MigrationActivity

--- app/src/main/java/app/notesr/activity/data/MigrationActivity.java
+++ app/src/main/java/app/notesr/activity/data/MigrationActivity.java
@@ -1,10 +1,21 @@
 package app.notesr.activity.data;
 
+import static app.notesr.service.android.AppMigrationAndroidService.EXTRA_COMPLETE;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import app.notesr.BuildConfig;
 import app.notesr.R;
+import app.notesr.activity.notes.NoteListActivity;
+import app.notesr.service.android.AppMigrationAndroidService;
+import app.notesr.service.migration.DataVersionManager;
 
 public class MigrationActivity extends AppCompatActivity {
 
@@ -12,5 +23,31 @@ public class MigrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_migration);
+
+        LocalBroadcastManager.getInstance(getApplicationContext())
+                .registerReceiver(broadcastReceiver(),
+                        new IntentFilter(AppMigrationAndroidService.BROADCAST_ACTION));
+
+        Intent serviceIntent = new Intent(getApplicationContext(), AppMigrationAndroidService.class);
+        startForegroundService(serviceIntent);
+    }
+
+    private BroadcastReceiver broadcastReceiver() {
+        return new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (AppMigrationAndroidService.BROADCAST_ACTION.equals(intent.getAction())) {
+                    boolean isCompleted = intent.getBooleanExtra(EXTRA_COMPLETE, false);
+
+                    if (isCompleted) {
+                        new DataVersionManager(getApplicationContext())
+                                .setCurrentVersion(BuildConfig.DATA_SCHEMA_VERSION);
+
+                        startActivity(new Intent(getApplicationContext(), NoteListActivity.class));
+                        finish();
+                    }
+                }
+            }
+        };
     }
 }
