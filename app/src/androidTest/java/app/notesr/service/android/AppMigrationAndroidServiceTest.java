1ff99e64b6747c6f8642d26b069e3a6ec25fc7d2
zHd4 <computersystembase@gmail.com>
2025-07-16 14:07:01 +0200

    Implement AppMigrationAndroidServiceTest

---
+++ app/src/androidTest/java/app/notesr/service/android/AppMigrationAndroidServiceTest.java
@@ -0,0 +1,52 @@
+package app.notesr.service.android;
+
+import static org.junit.Assert.assertTrue;
+import static app.notesr.service.android.AppMigrationAndroidService.EXTRA_COMPLETE;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+@RunWith(AndroidJUnit4.class)
+public class AppMigrationAndroidServiceTest {
+    private final Context context = ApplicationProvider.getApplicationContext();
+
+    @Test
+    public void testMigrationBroadcastSent() throws Exception {
+        IntentFilter filter = new IntentFilter(AppMigrationAndroidService.BROADCAST_ACTION);
+        CountDownLatch latch = new CountDownLatch(1);
+
+        boolean[] broadcastReceived = {false};
+
+        BroadcastReceiver receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                broadcastReceived[0] = intent.getBooleanExtra(EXTRA_COMPLETE, false);
+                latch.countDown();
+            }
+        };
+
+        LocalBroadcastManager.getInstance(context).registerReceiver(receiver, filter);
+
+        Intent intent = new Intent(context, AppMigrationAndroidService.class);
+        context.startService(intent);
+
+        boolean await = latch.await(5, TimeUnit.SECONDS);
+
+        assertTrue("Broadcast wasn't received", await);
+        assertTrue("EXTRA_COMPLETE must be true", broadcastReceived[0]);
+
+        LocalBroadcastManager.getInstance(context).unregisterReceiver(receiver);
+    }
+}
