0aa64ca55552380a06a4ac05342a1973f1709610
zHd4 <computersystembase@gmail.com>
2025-07-27 18:13:52 +0200

    Annotate TempFile as a Room Entity

--- app/src/main/java/app/notesr/model/TempFile.java
+++ app/src/main/java/app/notesr/model/TempFile.java
@@ -2,12 +2,17 @@ package app.notesr.model;
 
 import android.net.Uri;
 
+import androidx.room.Entity;
+
 import org.jetbrains.annotations.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
+@Entity
+@NoArgsConstructor
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Data
