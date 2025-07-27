f15043a82216c2680fced2027a011cd28d06d1a1
zHd4 <computersystembase@gmail.com>
2025-07-27 18:14:05 +0200

    Add TempFile entity and DAO to AppDatabase

--- app/src/main/java/app/notesr/db/AppDatabase.java
+++ app/src/main/java/app/notesr/db/AppDatabase.java
@@ -6,16 +6,19 @@ import androidx.room.RoomDatabase;
 import app.notesr.db.notes.dao.DataBlockDao;
 import app.notesr.db.notes.dao.FileInfoDao;
 import app.notesr.db.notes.dao.NoteDao;
+import app.notesr.db.service.dao.TempFileDao;
 import app.notesr.model.DataBlock;
 import app.notesr.model.FileInfo;
 import app.notesr.model.Note;
+import app.notesr.model.TempFile;
 
-@Database(entities = {Note.class, FileInfo.class, DataBlock.class},
+@Database(entities = {Note.class, FileInfo.class, DataBlock.class, TempFile.class},
         version = 1,
         exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
-    public abstract NoteDao noteDao();
-    public abstract FileInfoDao fileInfoDao();
-    public abstract DataBlockDao dataBlockDao();
+    public abstract NoteDao getNoteDao();
+    public abstract FileInfoDao getFileInfoDao();
+    public abstract DataBlockDao getDataBlockDao();
+    public abstract TempFileDao getTempFileDao();
 }
 
