fdfbd8d41165038efc591657be2920cf3c251d00
zHd4 <computersystembase@gmail.com>
2025-07-29 17:52:36 +0200

    Add DatabaseProvider

---
+++ app/src/main/java/app/notesr/db/DatabaseProvider.java
@@ -0,0 +1,33 @@
+package app.notesr.db;
+
+import android.content.Context;
+
+import androidx.room.Room;
+
+import net.sqlcipher.database.SQLiteDatabase;
+import net.sqlcipher.database.SupportFactory;
+
+import app.notesr.crypto.CryptoManager;
+
+public class DatabaseProvider {
+    private static final String DB_NAME = "notesr.db";
+    private static AppDatabase instance;
+
+    public static AppDatabase getInstance(Context context) {
+        if (instance == null) {
+            SQLiteDatabase.loadLibs(context);
+
+            CryptoManager cryptoManager = CryptoManager.getInstance(context);
+            byte[] passphrase = cryptoManager.getSecrets().getKey();
+
+            SupportFactory factory = new SupportFactory(passphrase);
+
+            instance = Room.databaseBuilder(context, AppDatabase.class, DB_NAME)
+                    .openHelperFactory(factory)
+                    .build();
+        }
+
+        return instance;
+    }
+}
+
