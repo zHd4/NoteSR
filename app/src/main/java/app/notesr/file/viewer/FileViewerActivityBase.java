64f8c48a67863d9a84f064e8629f566deaadd2bc
zHd4 <computersystembase@gmail.com>
2025-08-07 13:55:56 +0200

    Refactor FileViewerActivityBase

--- app/src/main/java/app/notesr/file/viewer/FileViewerActivityBase.java
+++ app/src/main/java/app/notesr/file/viewer/FileViewerActivityBase.java
@@ -2,10 +2,9 @@ package app.notesr.file.viewer;
 
 import static app.notesr.util.ActivityUtils.showToastMessage;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Looper;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -29,7 +28,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.security.NoSuchAlgorithmException;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class FileViewerActivityBase extends ActivityBase {
@@ -92,18 +90,21 @@ public class FileViewerActivityBase extends ActivityBase {
         return super.onOptionsItemSelected(item);
     }
 
-    private void executeTask(Runnable task, int dialogLayoutResId) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogTheme)
-                .setView(dialogLayoutResId)
-                .setCancelable(false);
+    private void executeTaskWithProgressDialog(Runnable task, Runnable postUiAction) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this,
+                R.style.AlertDialogTheme);
+        builder.setView(R.layout.progress_dialog_deleting).setCancelable(false);
 
         AlertDialog progressDialog = builder.create();
-        ExecutorService executor = Executors.newSingleThreadExecutor();
 
-        executor.execute(() -> {
+        Executors.newSingleThreadExecutor().execute(() -> {
             runOnUiThread(progressDialog::show);
             task.run();
             runOnUiThread(progressDialog::dismiss);
+
+            if (postUiAction != null) {
+                runOnUiThread(postUiAction);
+            }
         });
     }
 
@@ -117,79 +118,73 @@ public class FileViewerActivityBase extends ActivityBase {
     }
 
     protected void saveFileOnClick() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogTheme)
-                .setView(R.layout.dialog_are_you_sure)
-                .setTitle(R.string.warning)
-                .setPositiveButton(R.string.save, (dialog, result) -> saveFile())
-                .setNegativeButton(R.string.no, (dialog, result) -> {});
+        File destFile = Paths.get(saveDir.toPath().toString(), fileInfo.getName()).toFile();
 
-        builder.create().show();
+        Runnable task = () -> saveFile(destFile);
+
+        Runnable postUiAction = () -> {
+            String messageFormat = getResources().getString(R.string.saved_to);
+            String message = String.format(messageFormat, destFile.getAbsolutePath());
+            showToastMessage(this, message, Toast.LENGTH_LONG);
+        };
+
+        DialogInterface.OnClickListener onConfirm =
+                fileSavingConfirmedOnClick(task, postUiAction, destFile);
+
+        AlertDialog.Builder saveConfirmationDialogBuilder = new AlertDialog.Builder(this,
+                R.style.AlertDialogTheme);
+
+        saveConfirmationDialogBuilder.setView(R.layout.dialog_are_you_sure)
+                .setTitle(R.string.warning)
+                .setPositiveButton(R.string.save, onConfirm);
+
+        saveConfirmationDialogBuilder.create().show();
     }
 
-    private void saveFile() {
-        File destFile = Paths.get(saveDir.toPath().toString(), fileInfo.getName()).toFile();
-        Runnable saveRunnable = getSaveRunnable(destFile);
-
-        int dialogLayoutId = R.layout.progress_dialog_saving;
-
-        if (destFile.exists()) {
+    private DialogInterface.OnClickListener fileSavingConfirmedOnClick(Runnable task,
+                                                                       Runnable postUiAction,
+                                                                       File destFile) {
+        Runnable overwriteConfirmationDialog = () -> {
             AlertDialog.Builder builder =
-                    new AlertDialog.Builder(this, R.style.AlertDialogTheme)
-                            .setView(R.layout.dialog_file_already_exists)
-                            .setTitle(R.string.warning)
-                            .setPositiveButton(
-                                    R.string.overwrite,
-                                    (dialog, result) ->
-                                            executeTask(saveRunnable, dialogLayoutId))
-                            .setNegativeButton(
-                                    R.string.no,
-                                    (dialog, result) ->
-                                            showToastMessage(
-                                                    this,
-                                                    getResources().getString(R.string.saving_canceled),
-                                                    Toast.LENGTH_SHORT));
+                    new AlertDialog.Builder(this, R.style.AlertDialogTheme);
+
+            builder.setView(R.layout.dialog_file_already_exists)
+                    .setTitle(R.string.warning)
+                    .setPositiveButton(R.string.overwrite,
+                            (dialog, result) ->
+                                    executeTaskWithProgressDialog(task, postUiAction));
 
             builder.create().show();
-        } else {
-            executeTask(saveRunnable, dialogLayoutId);
-        }
-    }
+        };
 
-    private Runnable getSaveRunnable(File destFile) {
-        return () -> {
-            try {
-                fileService.read(fileInfo.getId(), chunk -> {
-                    try {
-                        FilesUtils.writeFileBytes(destFile, chunk, true);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-
-                String messageFormat = getResources().getString(R.string.saved_to);
-                String message = String.format(messageFormat, destFile.getAbsolutePath());
-
-                Looper.prepare();
-                showToastMessage(this, message, Toast.LENGTH_LONG);
-            } catch (RuntimeException e) {
-                Log.e("NoteSR", e.toString());
-
-                Looper.prepare();
-                showToastMessage(this, getResources().getString(R.string.cannot_save_file), Toast.LENGTH_SHORT);
+        return (dialog, result) -> {
+            if (!destFile.exists()) {
+                executeTaskWithProgressDialog(task, postUiAction);
+            } else {
+                overwriteConfirmationDialog.run();
             }
         };
     }
 
+    private void saveFile(File destFile) {
+        fileService.read(fileInfo.getId(), chunk -> {
+            try {
+                FilesUtils.writeFileBytes(destFile, chunk, true);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
     protected void deleteFileOnClick() {
+        DialogInterface.OnClickListener onConfirm = (dialog, result) ->
+                executeTaskWithProgressDialog(() ->
+                        fileService.delete(fileInfo.getId()), this::returnToListActivity);
+
         AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogTheme)
                 .setView(R.layout.dialog_action_cannot_be_undo)
                 .setTitle(R.string.warning)
-                .setNegativeButton(R.string.no, (dialog, result) -> {
-                })
-                .setPositiveButton(R.string.delete, (dialog, result) -> executeTask(() -> {
-                    fileService.delete(fileInfo.getId());
-                    returnToListActivity();
-                }, R.layout.progress_dialog_deleting));
+                .setPositiveButton(R.string.delete, onConfirm);
 
         builder.create().show();
     }
