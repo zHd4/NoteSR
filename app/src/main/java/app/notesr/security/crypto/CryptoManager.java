ca141b48ffb5882616afbed74ba1cfd078c06d02
zHd4 <computersystembase@gmail.com>
2025-08-12 14:29:25 +0200

    Remove trailing spaces from CryptoManager

--- app/src/main/java/app/notesr/security/crypto/CryptoManager.java
+++ app/src/main/java/app/notesr/security/crypto/CryptoManager.java
@@ -37,7 +37,7 @@ public class CryptoManager {
     private final SharedPreferences prefs;
 
     private CryptoSecrets secrets;
-    
+
     private Context getAppContext() {
         return App.getContext();
     }
