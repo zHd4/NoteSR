22d3d0f3765e811082b1c709f7954cc12bb220c6
zHd4 <computersystembase@gmail.com>
2025-08-22 20:59:47 +0200

    Add `RoomIntegrationMigration` to `AppMigrationRegistry`

--- app/src/main/java/app/notesr/migration/service/AppMigrationRegistry.java
+++ app/src/main/java/app/notesr/migration/service/AppMigrationRegistry.java
@@ -2,12 +2,13 @@ package app.notesr.migration.service;
 
 import java.util.List;
 
+import app.notesr.migration.changes.db.RoomIntegrationMigration;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class AppMigrationRegistry {
     public static List<AppMigration> getAllMigrations() {
-        return List.of();
+        return List.of(new RoomIntegrationMigration(1, 2));
     }
 }
