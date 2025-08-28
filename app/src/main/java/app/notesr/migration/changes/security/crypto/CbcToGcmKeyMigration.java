714524c0d70d118cd5961f99253345653a81d8b7
zHd4 <computersystembase@gmail.com>
2025-08-28 16:06:35 +0200

    Add key migration from CBC to GCM

---
+++ app/src/main/java/app/notesr/migration/changes/security/crypto/CbcToGcmKeyMigration.java
@@ -0,0 +1,30 @@
+package app.notesr.migration.changes.security.crypto;
+
+import android.content.Context;
+
+import app.notesr.exception.EncryptionFailedException;
+import app.notesr.migration.service.AppMigration;
+import app.notesr.migration.service.AppMigrationException;
+import app.notesr.security.crypto.CryptoManager;
+import app.notesr.security.crypto.CryptoManagerProvider;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CbcToGcmKeyMigration implements AppMigration {
+    @Getter
+    private final int fromVersion;
+
+    @Getter
+    private final int toVersion;
+
+    @Override
+    public void migrate(Context context) {
+        try {
+            CryptoManager cryptoManager = CryptoManagerProvider.getInstance();
+            cryptoManager.setSecrets(context, cryptoManager.getSecrets());
+        } catch (EncryptionFailedException e) {
+            throw new AppMigrationException("Failed to migrate key", e);
+        }
+    }
+}
--- app/src/main/java/app/notesr/migration/service/AppMigrationRegistry.java
+++ app/src/main/java/app/notesr/migration/service/AppMigrationRegistry.java
@@ -3,12 +3,16 @@ package app.notesr.migration.service;
 import java.util.List;
 
 import app.notesr.migration.changes.db.RoomIntegrationMigration;
+import app.notesr.migration.changes.security.crypto.CbcToGcmKeyMigration;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class AppMigrationRegistry {
     public static List<AppMigration> getAllMigrations() {
-        return List.of(new RoomIntegrationMigration(1, 2));
+        return List.of(
+                new CbcToGcmKeyMigration(1, 2),
+                new RoomIntegrationMigration(1, 2)
+        );
     }
 }
