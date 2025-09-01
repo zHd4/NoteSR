2001e85e76bcdbfad94d180195ce8b09e1b23090
zHd4 <computersystembase@gmail.com>
2025-09-01 18:49:03 +0200

    Refactor AesGcmCryptorOOMTest to use TemporaryFolder JUnit rule

--- app/src/androidTest/java/app/notesr/security/crypto/AesGcmCryptorOOMTest.java
+++ app/src/androidTest/java/app/notesr/security/crypto/AesGcmCryptorOOMTest.java
@@ -2,18 +2,15 @@ package app.notesr.security.crypto;
 
 import static org.junit.Assert.*;
 
-import android.content.Context;
-
-import androidx.test.core.app.ApplicationProvider;
-
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Random;
@@ -22,21 +19,20 @@ public class AesGcmCryptorOOMTest {
     private static final Random RANDOM = new Random();
     private static final String PASSWORD = "gcmPassword";
 
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
     private AesGcmCryptor cryptor;
-    private Path tempDir;
 
     @Before
     public void setUp() throws Exception {
-        Context context = ApplicationProvider.getApplicationContext();
-        tempDir = context.getCacheDir().toPath();
-
         byte[] salt = AesGcmCryptor.generatePasswordBasedSalt(PASSWORD);
         cryptor = new AesGcmCryptor(PASSWORD, salt);
     }
 
     @Test
     public void testLargeStreamEncryptionAndDecryption() throws Exception {
-        File largeFile = tempDir.resolve("large_file").toFile();
+        File largeFile = tempFolder.newFile("large_file");
 
         try (FileOutputStream out = new FileOutputStream(largeFile)) {
             for (int i = 0; i < 200; i++) {
@@ -47,14 +43,14 @@ public class AesGcmCryptorOOMTest {
             }
         }
 
-        File encryptedFile = tempDir.resolve("encrypted_file").toFile();
+        File encryptedFile = tempFolder.newFile("encrypted_file");
 
         FileInputStream plainIn = new FileInputStream(largeFile);
         FileOutputStream encryptedOut = new FileOutputStream(encryptedFile);
 
         cryptor.encrypt(plainIn, encryptedOut);
 
-        File decryptedFile = tempDir.resolve("decrypted_file").toFile();
+        File decryptedFile = tempFolder.newFile("decrypted_file");
 
         FileInputStream encryptedIn = new FileInputStream(encryptedFile);
         FileOutputStream decryptedOut = new FileOutputStream(decryptedFile);
