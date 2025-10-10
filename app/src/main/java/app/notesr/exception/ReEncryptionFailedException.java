0f80367bccf94e2097369a2a11d62910efccdc5e
zHd4 <computersystembase@gmail.com>
2025-10-10 14:31:22 +0200

    Remove unused ReEncryptionFailedException

--- app/src/main/java/app/notesr/exception/ReEncryptionFailedException.java
+++
@@ -1,7 +0,0 @@
-package app.notesr.exception;
-
-public class ReEncryptionFailedException extends RuntimeException {
-    public ReEncryptionFailedException(Exception e) {
-        super(e);
-    }
-}
