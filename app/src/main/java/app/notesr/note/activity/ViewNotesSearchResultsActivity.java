4a27f650c90b34d2be0f3a0615369ec0697f875c
zHd4 <computersystembase@gmail.com>
2025-10-11 14:32:41 +0200

    Make ViewNotesSearchResultsActivity final

--- app/src/main/java/app/notesr/note/activity/ViewNotesSearchResultsActivity.java
+++ app/src/main/java/app/notesr/note/activity/ViewNotesSearchResultsActivity.java
@@ -16,7 +16,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ViewNotesSearchResultsActivity extends ActivityBase {
+public final class ViewNotesSearchResultsActivity extends ActivityBase {
     private List<Note> results;
     private final Map<Long, String> notesIdsMap = new HashMap<>();
 
