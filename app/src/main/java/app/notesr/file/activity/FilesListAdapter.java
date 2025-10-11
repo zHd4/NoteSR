83e6ec17404876361d26058e989ee74f456674ff
zHd4 <computersystembase@gmail.com>
2025-10-11 16:13:13 +0200

    Refactor FilesListAdapter

--- app/src/main/java/app/notesr/file/activity/FilesListAdapter.java
+++ app/src/main/java/app/notesr/file/activity/FilesListAdapter.java
@@ -30,9 +30,9 @@ public final class FilesListAdapter extends ElementListAdapter<FileInfo> {
     );
 
     private static String toReadableSize(long size) {
-        String[] units = new String[] { "B", "KB", "MB", "GB", "TB", "PB", "EB" };
+        String[] units = new String[] {"B", "KB", "MB", "GB", "TB", "PB", "EB"};
 
-        if(size > 0) {
+        if (size > 0) {
             int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
             return new DecimalFormat("#,##0.#")
                     .format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
