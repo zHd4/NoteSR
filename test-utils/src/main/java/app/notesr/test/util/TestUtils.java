5fda7294489917d6089ae83cdf90dae153d6fc7d
zHd4 <computersystembase@gmail.com>
2025-10-13 13:17:55 +0200

    Add test-utils module

---
+++ test-utils/src/main/java/app/notesr/test/util/TestUtils.java
@@ -0,0 +1,13 @@
+package app.notesr.test.util;
+
+import java.nio.file.Path;
+
+public class TestUtils {
+    public static String getFixturePath(String pathPart) {
+        return Path.of("src/test/resources/fixtures", pathPart).toString();
+    }
+
+    public static String getTempPath(String pathPart) {
+        return Path.of(System.getProperty("java.io.tmpdir"), pathPart).toString();
+    }
+}
