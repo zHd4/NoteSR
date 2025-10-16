d8351a8de3aea663280429a9a4f6c14290f4a46e
zHd4 <computersystembase@gmail.com>
2025-10-17 01:54:29 +0200

    Add @Ignore to FileBlobInfo constructor

--- data/src/main/java/app/notesr/data/model/FileBlobInfo.java
+++ data/src/main/java/app/notesr/data/model/FileBlobInfo.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
+import androidx.room.Ignore;
 import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
@@ -22,7 +23,7 @@ import lombok.NoArgsConstructor;
         indices = {@Index("file_id")}
 )
 @NoArgsConstructor
-@AllArgsConstructor
+@AllArgsConstructor(onConstructor_ = @Ignore)
 public final class FileBlobInfo {
 
     @PrimaryKey
