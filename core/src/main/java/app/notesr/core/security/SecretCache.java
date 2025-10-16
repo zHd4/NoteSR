b05d6bd9c7eae96771fa9dbcc5b2c6f3a97ba8be
zHd4 <computersystembase@gmail.com>
2025-10-16 18:00:07 +0200

    Refactor SecretCache

--- core/src/main/java/app/notesr/core/security/SecretCache.java
+++ core/src/main/java/app/notesr/core/security/SecretCache.java
@@ -8,14 +8,14 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class SecretCache {
-    private static final ConcurrentHashMap<String, byte[]> map = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<String, byte[]> MAP = new ConcurrentHashMap<>();
 
     public static void put(String key, byte[] value) {
-        map.put(key, value);
+        MAP.put(key, value);
     }
 
     public static byte[] take(String key) {
-        byte[] valueInMap = map.get(key);
+        byte[] valueInMap = MAP.get(key);
 
         if (valueInMap == null) {
             return null;
@@ -24,13 +24,13 @@ public final class SecretCache {
         byte[] value = Arrays.copyOf(valueInMap, valueInMap.length);
 
         Arrays.fill(valueInMap, (byte) 0);
-        map.remove(key);
+        MAP.remove(key);
 
         return value;
     }
 
     public static void clear() {
-        map.values().forEach(arr -> Arrays.fill(arr, (byte) 0));
-        map.clear();
+        MAP.values().forEach(arr -> Arrays.fill(arr, (byte) 0));
+        MAP.clear();
     }
 }
