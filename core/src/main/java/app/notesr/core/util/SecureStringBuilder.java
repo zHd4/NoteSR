ae8e58bcc10c162580492fed529005fb389a4e3e
zHd4 <computersystembase@gmail.com>
2025-10-16 17:54:24 +0200

    Add SecureStringBuilder

---
+++ core/src/main/java/app/notesr/core/util/SecureStringBuilder.java
@@ -0,0 +1,105 @@
+package app.notesr.core.util;
+
+import androidx.annotation.NonNull;
+
+import java.util.Arrays;
+
+public final class SecureStringBuilder {
+    private static final int INITIAL_CAPACITY = 32;
+
+    private char[] buffer;
+    private int length = 0;
+
+    public SecureStringBuilder() {
+        this(INITIAL_CAPACITY);
+    }
+
+    public SecureStringBuilder(int initialCapacity) {
+        if (initialCapacity <= 0) {
+            throw new IllegalArgumentException("Capacity must be > 0");
+        }
+
+        buffer = new char[initialCapacity];
+    }
+
+    public SecureStringBuilder append(char c) {
+        ensureCapacity(length + 1);
+        buffer[length++] = c;
+
+        return this;
+    }
+
+    public SecureStringBuilder append(char[] chars) {
+        if (chars == null) {
+            return this;
+        }
+
+        ensureCapacity(length + chars.length);
+        System.arraycopy(chars, 0, buffer, length, chars.length);
+        length += chars.length;
+
+        return this;
+    }
+
+    public SecureStringBuilder append(CharSequence seq) {
+        if (seq == null) {
+            return this;
+        }
+
+        ensureCapacity(length + seq.length());
+
+        for (int i = 0; i < seq.length(); i++) {
+            buffer[length++] = seq.charAt(i);
+        }
+
+        return this;
+    }
+
+    public SecureStringBuilder deleteCharAt(int index) {
+        if (index < 0 || index >= length) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+
+        System.arraycopy(buffer, index + 1, buffer, index, length - index - 1);
+        buffer[length - 1] = '\0';
+        length--;
+        return this;
+    }
+
+    public char[] toCharArray() {
+        return Arrays.copyOf(buffer, length);
+    }
+
+    public int length() {
+        return length;
+    }
+
+    public void wipe() {
+        if (buffer != null) {
+            Arrays.fill(buffer, '\0');
+            buffer = new char[0];
+            length = 0;
+        }
+    }
+
+    private void ensureCapacity(int minCapacity) {
+        if (minCapacity <= buffer.length) {
+            return;
+        }
+
+        int newCapacity = Math.max(buffer.length * 2, minCapacity);
+        char[] newBuffer = new char[newCapacity];
+
+        System.arraycopy(buffer, 0, newBuffer, 0, length);
+        Arrays.fill(buffer, '\0');
+
+        buffer = newBuffer;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return new String(buffer, 0, length);
+    }
+}
+
---
+++ core/src/test/java/app/notesr/core/util/SecureStringBuilderTest.java
@@ -0,0 +1,146 @@
+package app.notesr.core.util;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class SecureStringBuilderTest {
+    @Test
+    void testAppendChar() {
+        SecureStringBuilder builder = new SecureStringBuilder();
+        builder.append('A');
+
+        assertEquals(1, builder.length(),
+                "Length should be 1 after appending one char");
+        assertArrayEquals(new char[]{'A'}, builder.toCharArray(),
+                "Char array should contain the appended char");
+    }
+
+    @Test
+    void testAppendCharArray() {
+        SecureStringBuilder builder = new SecureStringBuilder();
+        builder.append(new char[]{'a', 'b', 'c'});
+
+        assertEquals(3, builder.length(),
+                "Length should match number of appended chars");
+        assertArrayEquals(new char[]{'a', 'b', 'c'}, builder.toCharArray(),
+                "Char array should contain appended chars");
+    }
+
+    @Test
+    void testAppendCharSequence() {
+        SecureStringBuilder builder = new SecureStringBuilder();
+        builder.append("test");
+
+        assertEquals(4, builder.length(),
+                "Length should equal the appended CharSequence length");
+        assertArrayEquals(new char[]{'t', 'e', 's', 't'}, builder.toCharArray(),
+                "Buffer should match appended sequence");
+    }
+
+    @Test
+    void testAppendNullCharArrayDoesNothing() {
+        SecureStringBuilder builder = new SecureStringBuilder();
+        builder.append((char[]) null);
+
+        assertEquals(0, builder.length(),
+                "Appending null char array should not change length");
+    }
+
+    @Test
+    void testAppendNullCharSequenceDoesNothing() {
+        SecureStringBuilder builder = new SecureStringBuilder();
+        builder.append((CharSequence) null);
+
+        assertEquals(0, builder.length(),
+                "Appending null CharSequence should not change length");
+    }
+
+    @Test
+    void testEnsureCapacityExpandsBuilderBuffer() {
+        SecureStringBuilder builder = new SecureStringBuilder(1);
+        builder.append("abcd");
+
+        assertTrue(builder.length() >= 4,
+                "Buffer should expand to fit appended chars");
+        assertEquals("abcd", builder.toString(),
+                "String representation should match appended text");
+    }
+
+    @Test
+    void testToCharArrayReturnsCopy() {
+        SecureStringBuilder builder = new SecureStringBuilder();
+        builder.append("xyz");
+
+        char[] arr = builder.toCharArray();
+        arr[0] = 'A';
+
+        assertEquals('x', builder.toCharArray()[0],
+                "Modifying returned array should not affect internal buffer");
+    }
+
+    @Test
+    void testToStringCreatesNewString() {
+        SecureStringBuilder builder = new SecureStringBuilder();
+        builder.append("abc");
+
+        assertEquals("abc", builder.toString(),
+                "toString() should return correct text representation");
+    }
+
+    @Test
+    void testWipeClearsData() {
+        SecureStringBuilder builder = new SecureStringBuilder();
+        builder.append("secret");
+        builder.wipe();
+        assertEquals(0, builder.length(), "Length should be 0 after wipe");
+        assertArrayEquals(new char[0], builder.toCharArray(),
+                "Char array should be empty after wipe");
+    }
+
+    @Test
+    void testIllegalInitialCapacityThrowsException() {
+        assertThrows(IllegalArgumentException.class, () -> new SecureStringBuilder(0),
+                "Creating builder with zero capacity should throw IllegalArgumentException");
+    }
+
+    @Test
+    void testDeleteCharAtRemovesCharacter() {
+        SecureStringBuilder builder = new SecureStringBuilder();
+        builder.append("abc");
+        builder.deleteCharAt(1);
+
+        assertEquals(2, builder.length(),
+                "Length should decrease after deleting a char");
+        assertEquals("ac", builder.toString(),
+                "Character at index should be removed");
+    }
+
+    @Test
+    void testDeleteCharAtFirstAndLast() {
+        SecureStringBuilder builder = new SecureStringBuilder();
+        builder.append("abcd");
+
+        builder.deleteCharAt(0);
+        assertEquals("bcd", builder.toString(),
+                "First character should be removed");
+
+        builder.deleteCharAt(builder.length() - 1);
+        assertEquals("bc", builder.toString(),
+                "Last character should be removed");
+    }
+
+    @Test
+    void testDeleteCharAtInvalidIndexThrows() {
+        SecureStringBuilder builder = new SecureStringBuilder();
+        builder.append("abc");
+
+        assertThrows(IndexOutOfBoundsException.class, () -> builder.deleteCharAt(-1),
+                "Negative index should throw");
+        assertThrows(IndexOutOfBoundsException.class, () -> builder.deleteCharAt(3),
+                "Index equal to length should throw");
+    }
+}
