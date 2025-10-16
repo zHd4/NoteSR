4ba9965db5c42f97c1a92cf7cdc19827df2aad10
zHd4 <computersystembase@gmail.com>
2025-10-16 17:59:59 +0200

    Refactor KeyUtils

--- core/src/main/java/app/notesr/core/util/KeyUtils.java
+++ core/src/main/java/app/notesr/core/util/KeyUtils.java
@@ -155,9 +155,18 @@ public final class KeyUtils {
     }
 
     private static int hexDigitToInt(char c) {
-        if (c >= '0' && c <= '9') return c - '0';
-        if (c >= 'a' && c <= 'f') return c - 'a' + 10;
-        if (c >= 'A' && c <= 'F') return c - 'A' + 10;
+        if (c >= '0' && c <= '9') {
+            return c - '0';
+        }
+
+        if (c >= 'a' && c <= 'f') {
+            return c - 'a' + 10;
+        }
+
+        if (c >= 'A' && c <= 'F') {
+            return c - 'A' + 10;
+        }
+
         return -1;
     }
 }
