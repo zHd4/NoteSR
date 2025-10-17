60c49b390da50492c1df32c8cfed1846bbdadd9a
zHd4 <computersystembase@gmail.com>
2025-10-17 02:36:43 +0200

    Make TestUtils final

--- core/src/main/java/app/notesr/core/util/TestUtils.java
+++ core/src/main/java/app/notesr/core/util/TestUtils.java
@@ -2,7 +2,7 @@ package app.notesr.core.util;
 
 import java.nio.file.Paths;
 
-public class TestUtils {
+public final class TestUtils {
     public static String getFixturePath(String pathPart) {
         return Paths.get("src/test/resources/fixtures", pathPart).toString();
     }
