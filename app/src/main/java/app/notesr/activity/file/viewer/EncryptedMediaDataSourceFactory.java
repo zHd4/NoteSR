c488f6a332739f218c7da94c9d539eaa892a51d6
zHd4 <computersystembase@gmail.com>
2025-10-22 16:33:50 +0200

    Add EncryptedMediaDataSourceFactory

---
+++ app/src/main/java/app/notesr/activity/file/viewer/EncryptedMediaDataSourceFactory.java
@@ -0,0 +1,59 @@
+package app.notesr.activity.file.viewer;
+
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.media3.common.util.UnstableApi;
+import androidx.media3.datasource.DataSource;
+import androidx.media3.datasource.DataSpec;
+import androidx.media3.datasource.TransferListener;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import app.notesr.core.security.crypto.AesCryptor;
+
+@UnstableApi
+public final class EncryptedMediaDataSourceFactory implements DataSource.Factory {
+    private final AesCryptor cryptor;
+    private final List<File> blockFiles;
+    private final int cacheBlocks;
+
+    public EncryptedMediaDataSourceFactory(
+            AesCryptor cryptor,
+            List<File> blockFiles,
+            int cacheBlocks) {
+
+        this.cryptor = cryptor;
+        this.blockFiles = blockFiles;
+        this.cacheBlocks = Math.max(1, cacheBlocks);
+    }
+
+    @NonNull
+    @Override
+    public DataSource createDataSource() {
+        try {
+            return new EncryptedMediaDataSource(cryptor, blockFiles, cacheBlocks);
+        } catch (IOException e) {
+            return new DataSource() {
+                @Override
+                public void addTransferListener(@NonNull TransferListener transferListener) {
+                }
+
+                @Override
+                public long open(@NonNull DataSpec dataSpec) throws IOException {
+                    throw new IOException("Failed to create EncryptedMediaDataSource", e);
+                }
+
+                @Override public int read(@NonNull byte[] buffer, int offset, int readLength)
+                        throws IOException { throw new IOException("Not opened"); }
+
+                @Nullable
+                @Override public Uri getUri() { return null; }
+                @Override public void close() {}
+            };
+        }
+    }
+}
