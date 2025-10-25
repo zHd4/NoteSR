9bd3da4f75b24731c5be71d59c43de509340627d
zHd4 <computersystembase@gmail.com>
2025-10-25 15:44:30 +0200

    Refactor ImportAndroidService

--- service/src/main/java/app/notesr/service/importer/ImportAndroidService.java
+++ service/src/main/java/app/notesr/service/importer/ImportAndroidService.java
@@ -54,13 +54,13 @@ public final class ImportAndroidService extends Service implements Runnable {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME,
+        NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME,
                 NotificationManager.IMPORTANCE_NONE);
 
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID).build();
         NotificationManager notificationManager = getSystemService(NotificationManager.class);
 
-        notificationManager.createNotificationChannel(channel);
+        notificationManager.createNotificationChannel(notificationChannel);
 
         int type = 0;
 
