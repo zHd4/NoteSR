3963638661418283a51abea99aa5f3abaebda580
zHd4 <computersystembase@gmail.com>
2025-10-25 15:04:39 +0200

    Remove TempFile and TempFileDao from AppDatabase

--- data/src/main/java/app/notesr/data/AppDatabase.java
+++ data/src/main/java/app/notesr/data/AppDatabase.java
@@ -7,20 +7,17 @@ import androidx.room.TypeConverters;
 import app.notesr.data.dao.FileBlobInfoDao;
 import app.notesr.data.dao.FileInfoDao;
 import app.notesr.data.dao.NoteDao;
-import app.notesr.data.dao.TempFileDao;
 import app.notesr.data.model.FileBlobInfo;
 import app.notesr.data.model.FileInfo;
 import app.notesr.data.model.Note;
-import app.notesr.data.model.TempFile;
 
-@Database(entities = {Note.class, FileInfo.class, FileBlobInfo.class, TempFile.class},
-        version = 1,
+@Database(entities = {Note.class, FileInfo.class, FileBlobInfo.class},
+        version = 2,
         exportSchema = false)
 @TypeConverters({DatabaseTypeConverters.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract NoteDao getNoteDao();
     public abstract FileInfoDao getFileInfoDao();
     public abstract FileBlobInfoDao getFileBlobInfoDao();
-    public abstract TempFileDao getTempFileDao();
 }
 
