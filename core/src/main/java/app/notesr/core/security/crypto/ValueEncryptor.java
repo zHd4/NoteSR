e74c3b8ecda1d43dd65e83a418865898fefaafa3
zHd4 <38856321+zHd4@users.noreply.github.com>
2026-04-23 13:21:48 +0200

    Add ValueEncryptor class for byte array and String encryption

---
+++ core/src/main/java/app/notesr/core/security/crypto/ValueEncryptor.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2026 zHd4
+ * SPDX-License-Identifier: MIT
+ */
+
+package app.notesr.core.security.crypto;
+
+import java.security.GeneralSecurityException;
+import java.util.Base64;
+
+import app.notesr.core.security.exception.EncryptionFailedException;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public final class ValueEncryptor {
+
+    private final AesCryptor cryptor;
+
+    public byte[] encrypt(byte[] plainValue) throws EncryptionFailedException {
+        try {
+            return cryptor.decrypt(plainValue);
+        } catch (GeneralSecurityException e) {
+            throw new EncryptionFailedException(e);
+        }
+    }
+
+    public String encrypt(String plainValue) throws EncryptionFailedException {
+        byte[] encryptedValue = encrypt(plainValue.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedValue);
+    }
+}
