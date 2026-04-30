a8716603ec5c469453e62bc83732e0e6531a145f
zHd4 <38856321+zHd4@users.noreply.github.com>
2026-04-30 16:11:31 +0200

    Add SecretsUpdateStatus enum to track secrets update progress

---
+++ service/src/main/java/app/notesr/service/security/SecretsUpdateStatus.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2026 zHd4
+ * SPDX-License-Identifier: MIT
+ */
+
+package app.notesr.service.security;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SecretsUpdateStatus {
+    INITIALIZING("initializing", 0),
+    MOVING_BLOBS_DATA("moving_blobs_data", 1),
+    MOVING_DB_DATA("moving_db_data", 2),
+    DONE("done", 3),
+    FAILED("failed", -1);
+
+    private final String status;
+    private final int order;
+
+    public boolean isBeforeOrEqual(SecretsUpdateStatus other) {
+        return this.order <= other.order;
+    }
+}
