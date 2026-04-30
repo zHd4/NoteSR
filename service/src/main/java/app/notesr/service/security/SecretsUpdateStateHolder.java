1e31a79743347cab9b8000c446d5e1bff68c2c86
zHd4 <38856321+zHd4@users.noreply.github.com>
2026-04-30 16:19:05 +0200

    Add SecretsUpdateStateHolder to manage and notify state updates

---
+++ service/src/main/java/app/notesr/service/security/SecretsUpdateStateHolder.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2026 zHd4
+ * SPDX-License-Identifier: MIT
+ */
+
+package app.notesr.service.security;
+
+import java.util.function.Consumer;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public final class SecretsUpdateStateHolder {
+
+    private final Consumer<SecretsUpdateState> onUpdate;
+
+    private SecretsUpdateState state = new SecretsUpdateState();
+
+    public SecretsUpdateState getState() {
+        return SecretsUpdateState.from(state);
+    }
+
+    public SecretsUpdateStateHolder setState(SecretsUpdateState newState) {
+        if (newState == null) {
+            return this;
+        }
+
+        state = SecretsUpdateState.from(newState);
+        onUpdate.accept(state);
+
+        return this;
+    }
+}
